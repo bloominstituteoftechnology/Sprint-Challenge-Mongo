705e9a466312345e119c2e586c280da721df5a36
Tyler Sanford <tylersanford0311@gmail.com>
2017-12-01 15:12:42 -0600

    First PR

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,3 +1,17 @@
+const budgetControllers = require('../controllers/budgetControllers');
+const categoryControllers = require('../controllers/categoryControllers');
+const expenseControllers = require('../controllers/expenseControllers');
+
 module.exports = app => {
-  // Todo: Fill in your routes here
+  app.route('/budget').post(budgetControllers.budgetCreate); // Post method on /budget to create new budget
+
+  app
+    .route('/category')
+    .post(categoryControllers.categoryCreate)
+    .get(categoryControllers.categoryListAll);
+
+  app
+    .route('/expense')
+    .post(expenseControllers.expenseCreate)
+    .get(expenseControllers.expenseListAll);
 };
