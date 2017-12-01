eafda439cd30a5cfec9f54529035224cb6a30dce
Austin F <austindanielfrench@gmail.com>
2017-12-01 10:31:03 -0800

    routes and models near done

---
+++ api/models/expense.js
@@ -0,0 +1,7 @@
+// {
+//   _id: '543d2c72gsb23cd657438921',
+//   title: 'Groceries',
+// }
+
+const mongoose = require('mongoose');
+module.exports = mongoose.model('Expense', ExpenseSchema);
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,3 +1,13 @@
 module.exports = app => {
-  // Todo: Fill in your routes here
+  app.route('/budget').post(controller.newBudget);
+  app
+    .route('/category')
+    .post(controller.newCategory)
+    .get(controller.returnCategories);
+  app
+    .route('/expense')
+    .post(controller.newExpense)
+    .get(controller.returnExpenses);
+  // app.route('/budget/:id/summary')
+  // app.route('/expenses?aggregatedBy=category')
 };
