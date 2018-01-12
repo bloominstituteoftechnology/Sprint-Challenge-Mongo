a6ea844580ef661e5cc0d4e59e58e43a47153090
Jesse Hood <jessenoob@gmail.com>
2018-01-12 13:56:53 -0500

    comment out aggregate attempt

--- api/controllers/budgetControllers.js
+++ api/controllers/budgetControllers.js
@@ -1,4 +1,5 @@
 const Budget = require('../models/budget');
+const Expense = require('../models/expense');
 
 async function createBudget(req, res) {
   try {
@@ -9,7 +10,35 @@ async function createBudget(req, res) {
     res.status(422).json({err: err.message});
   }
 }
+/* 
+function getBudgetSummary(req, res) {
+  const { id } = req.params;
+  function aggregateCallback(err, result) {
+    if (err) {
+      console.log(err);
+      return res.status(422).json({err: err.message});
+    }
+    console.log(result);
+    res.status(200).json(result);
+  }
 
+  Budget.aggregate([
+    { $match: {
+      _id: id
+    }},
+    { $lookup: {
+      from: 'expense', localField: 'expense', foreignField: '_id', as: 'expenses'
+    }},
+    { $group: 
+      {
+        _id: '$expenses._id',
+        total_expenses: { $sum: '$expenses.amount'}
+      }
+    }
+  ], aggregateCallback);
+}
+*/
 module.exports = {
-  createBudget
+  createBudget,
+  // getBudgetSummary
 }
