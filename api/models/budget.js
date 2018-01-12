2bb104d73f9b64d22eec6f368d0e8ca6e67dfa13
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-12 11:46:46 -0500

    Models Setup

---
+++ api/models/budget.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const BudgetSchema = new mongoose.Schema({
+  title: String,
+  budgetAmount: Number
+});
+
+module.exports = mongoose.model('Budget', Budget);
---
+++ api/models/category.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const CatergorySchema = mongoose.Schema({
+  title: String
+});
+
+module.exports = mongoose.model('Category', Category);
