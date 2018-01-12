42cf5030aac0aab31aa28701428ab54151f5608b
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-12 13:10:26 -0500

    All routes and controllers working

---
+++ api/controllers/expenseControllers.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Expense = require('../models/expense');
+
+const getExpenses = (req, res) => {
+  Expense.find({})
+    .then(expenses => {
+      res.status(200).json(expenses);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error retrieving expenses', err });
+    });
+};
+
+const createExpense = (req, res) => {
+  const expense = new Expense(req.body);
+
+  expense
+    .save()
+    .then(newExpense => {
+      res.status(201).json(newExpense);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error createing expense', err });
+    });
+};
+
+module.exports = {
+  getExpenses,
+  createExpense
+};
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const budgetControllers = require('../controllers/budgetControllers');
 const categoryControllers = require('../controllers/categoryControllers');
+const expenseControllers = require('../controllers/expenseControllers');
 
 module.exports = app => {
   const router = express.Router();
@@ -11,6 +12,10 @@ module.exports = app => {
   router.route('/category').post(categoryControllers.createCategory);
 
   router.route('/category').get(categoryControllers.getCategories);
+
+  router.route('/expense').post(expenseControllers.createExpense);
+
+  router.route('/expense').get(expenseControllers.getExpenses);
 
   app.use('/api', router);
 };
