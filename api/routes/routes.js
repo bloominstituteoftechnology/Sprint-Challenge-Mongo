d76e5a3d06cb9cc58ff10a8f5e7578fe842143a7
Clara Andrew <candrewwani@gmail.com>
2018-02-23 17:28:50 -0500

    added budget summary
    api/controllers/budgetControllers.js: added controller to get budget summary.
    
    api/routes/routes.js: added route for budget summary.

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,13 +1,14 @@
 module.exports = app => {
   // Todo: Fill in your routes here
-  const createBudget = require('../controllers/budgetControllers');
+  const budget = require('../controllers/budgetControllers');
   const category = require('../controllers/categoryControllers');
   const expense = require('../controllers/expenseControllers');
 
-  app.post('/budget', createBudget);
+  app.post('/budget', budget.budgetCreate);
   app.post('/category', category.createCategory);
   app.get('/category', category.categories);
   app.post('/expense', expense.createExpense);
   app.get('/expense', expense.expenses);
+  app.get('/budget/:id/summary', budget.budgetSummary);
 
 };
