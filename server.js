c4c8a2d4a6e450c80cbeb3ce960e3f743a34d0ef
Johnathan Justinn <jjustinn85@gmail.com>
2018-02-23 17:00:16 -0500

    Budget Post endpoint created

--- server.js
+++ server.js
@@ -19,6 +19,27 @@ server.get("/", (req, res) => {
   res.status(200).json({ api: "The API rises from its slumber" });
 });
 
+server.post("/budget", (req, res) => {
+  const { title, startingAmount } = req.body;
+  const budget = new Budget(req.body);
+  if (title && startingAmount) {
+    budget
+      .save()
+      .then(savedBudget => {
+        res.status(201).json(savedBudget);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({
+            error: "There was an error saving this budget to the database"
+          });
+      });
+  } else {
+    res.status(404).json({ error: "Please provide a budget title and amount" });
+  }
+});
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
