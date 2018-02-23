8bd58019842df23d373f461f8c69c3b9e1fdb63b
Samuel Cha <sunchipssam@gmail.com>
2018-02-23 11:53:46 -0800

    Finished budget

---
+++ api/controllers/budgetControllers.js
@@ -0,0 +1,7 @@
+const Budget = require('../models/budget');
+
+const createBudget = budgetInfo => {
+  return new Budget(budgetInfo).save();
+};
+
+module.exports = createBudget;
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,5 +1,11 @@
+const budgetController = require('../controllers/budgetControllers');
+
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.status(200).send('a');
+  app.post('/budget', (req, res) => {
+    const { title, budgetAmount } = req.body;
+
+    budgetController({ title, budgetAmount })
+      .then(savedBudget => res.status(200).send(savedBudget))
+      .catch(err => res.status(500).send(err));
   });
 };
