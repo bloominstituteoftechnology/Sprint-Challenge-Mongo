fbfbcb7af787c21d1d909f2b9ae2270effa320a7
Johnathan Justinn <jjustinn85@gmail.com>
2018-02-23 13:53:52 -0500

    Ready to go with all dependencies installed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const server = express();
 
 const port = process.env.PORT || 3000;
@@ -12,6 +13,10 @@ mongoose.connect('mongodb://localhost/budget', { useMongoClient: true });
 server.use(bodyParser.json());
 
 routes(server);
+
+server.get('/', (req, res)=> {
+  res.status(200).json({ api: 'The API rises from its slumber' });
+});
 
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
