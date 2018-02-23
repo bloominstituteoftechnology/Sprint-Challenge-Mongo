1611900e0fa05aef488aabe90c9e36a1fcb3fd0b
Anthony Ellis <aellisnola@gmail.com>
2018-02-23 16:01:16 -0600

    One question answered. Schema Set. Server working. Post-Budget working.

--- server.js
+++ server.js
@@ -3,15 +3,39 @@ const express = require('express');
 const mongoose = require('mongoose');
 const server = express();
 
+server.use(bodyParser.json());
+
+const Budget = require('./api/models/budget.js');
+const Expense = require('./api/models/expense.js');
+const Category = require('./api/models/category.js');
+
 const port = process.env.PORT || 3000;
 
 const routes = require('./api/routes/routes');
+
+routes(server);
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/budget', { useMongoClient: true });
 
 server.use(bodyParser.json());
 
-routes(server);
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'Up and running! '})
+})
+
+server.post('/budget', (req, res) => {
+  const { title, budgetAmount } = req.body;
+  const budget = new Budget(req.body);
+
+  Budget.create(budget)
+  .then(savedBudget => {
+    res.send(savedBudget)
+  })
+  .catch(error => {
+    res.send({ error: "Could not create new budget." });
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
