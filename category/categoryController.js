258638b60409b37c74996a3141e24b7b993653d3
Arkoma <aaron@adburk.net>
2018-03-16 12:46:02 -0500

    category controller complete

--- category/categoryController.js
+++ category/categoryController.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const CategoryRouter = express.Router();
-const CategoryModel = ('./CategoryModel');
+const Category = require('./CategoryModel');
 
+CategoryRouter.post('/', (req,res) => {
+  const CategoryInfo = req.body;
+  const newCategory = new Category(CategoryInfo);
+  newCategory
+    .save()
+    .then(savedCategory => {
+      res.status(200).json(savedCategory);
+    })
+    .catch(err => {
+      res.status(500).json({error: 'There was an error saving your category to the database'})
+    })
+})
+
+CategoryRouter.get('/', (req,res) => {
+  Category.find({})
+    .select('title')
+    .then(categories => {
+      res.status(200).json(categories);
+    })
+    .catch(err => {
+      res.status(500).json({error: 'There was an error retrieving your categories from the database'})
+    })
+});
 
 module.exports = CategoryRouter;
--- expense/expenseController.js
+++ expense/expenseController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const ExpenseRouter = express.Router();
-const ExpenseModel = ('./ExpenseModel');
+const ExpenseModel = require('./ExpenseModel');
+
+
 
 module.exports = ExpenseRouter;
