09336258760365a1091a2e99d4e3c545f2ad5fa0
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-16 11:39:33 -0400

    Set up server.js
    server.js --- Basic server set-up.  Server and Mongo connection working.  More or less just need router which I have yet to write.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express'); // remember to install your npm packages
+const mongoose = require('mongoose');
 
 const server = express();
 
 // add your server code
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'Running' });
+});
+
+mongoose
+  .connect('mongodb://localhost/budgettracker')
+  .then(connetion => console.log('Connected to Mongo'))
+  .catch(error => console.log('Error Connecting to Mongo'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
