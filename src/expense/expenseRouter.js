24ed0f163cb7b36344dae85d71b0da8d861d2905
Peter Gray <peter@petergraycreative.com>
2018-03-16 12:49:24 -0500

    Created router stretch endpoint at /expense, returns budget diff
    Created the endpoint at /expense that returns the difference between the budget and the expense.
    
    To move it to the correct endpoint wouldnt be a problem though as you would just do the same thing. 
    
    I did have an issue with reassigning Expense after it is compiled when I imported it into the Budget router and I wasnt able to solve that.

--- src/expense/expenseRouter.js
+++ src/expense/expenseRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Expense = require('./expenseModel.js');
+const Budget = require('../budget/BudgetModel.js');
 
 const router = express.Router();
 
@@ -18,13 +19,43 @@ router.post('/', (req, res) => {
           .status(500)
           .json({ error: 'There was a problem adding the expense.' })
       );
-  } else res.status(400).json({ error: 'Please provide a budget ID, category ID, and amount.' });
+  } else
+    res
+      .status(400)
+      .json({ error: 'Please provide a budget ID, category ID, and amount.' });
 });
 
 router.get('/', (req, res) => {
   Expense.find()
+    .populate('budget', 'title')
+    .populate('category', 'title')
     .then((expenses) => res.status(200).json(expenses))
     .catch((err) => res.status(500).json({ err: err.message }));
+});
+
+router.get('/:id/summary', (req, res) => {
+  const id = req.params.id;
+  Expense.aggregate([
+    {
+      $group: {
+        _id: null,
+        expenseTotal: { $sum: '$amount' },
+      },
+    },
+  ]).then((sum) => {
+    Budget.findById(id)
+    .then(budget => {
+      const difference = budget.budgetAmount - sum[0].expenseTotal;
+      const budgetSummary = {
+        BudgetSummary: {
+        budget: budget.budgetAmount,
+        expenses: sum[0].expenseTotal,
+        difference: difference,
+        }
+      }
+      res.status(200).json(budgetSummary)
+    })
+  });
 });
 
 module.exports = router;
