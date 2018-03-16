1491d9b47987f6629e51051a6d58aad755ee3f08
shobanavv <shobanavv@gmail.com>
2018-03-16 10:29:26 -0700

    WIP - Post endpoints for all models

---
+++ routes/categoryrouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const Category = require('../models/Category');
+const router = express.Router();
+
+router.post('/',(req, res) => {
+
+});
+module.exports = router;
---
+++ routes/expenserouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const Expense = require('../models/Expense');
+const router = express.Router();
+router.post('/',(req, res) => {
+
+});
+module.exports = router;
