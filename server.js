64a3470fea7c79286d4c5fa9b00a7bd026e1ec46
Richard Reis <richardreeze1@gmail.com>
2018-03-16 10:13:04 -0700

    Add router files + modify gitignore... Now the real fun begins

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const server = express();
 server.use(helmet());
 server.use(bodyParser.json);
 
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
