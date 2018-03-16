525b1595634c3d39cd97f3602249218281707c5c
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-16 14:05:17 -0500

    Syntax Cleanup
    in expense.js:
    change descritpion type to String

--- Expense/expense.js
+++ Expense/expense.js
@@ -3,7 +3,7 @@ const objectID = mongoose.Schema.Types.ObjectId;
 
 const Expense = new mongoose.Schema({
   amount: { type: Number, required: true },
-  description: { type: Number, required: true },
+  description: { type: String, required: true },
   budget: [{ type: objectID, ref: 'Budget' }],
   category: [{ type: objectID, ref: 'Category'}],
 });
