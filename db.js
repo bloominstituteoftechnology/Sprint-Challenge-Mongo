e0ab90edcc3558bc0fb3d93228298d9161ef855f
german gonzalez <gcomlnk@gmail.com>
2018-03-16 13:41:58 -0400

    installing dependencies creating db && connecting to it.

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+    connectTo: function (database = 'btracker', host = 'germancin:secure123@167.99.10.46') {
+        return mongoose.connect(`mongodb://${host}/${database}`)
+            .then(conn => console.log(`Connected to MongoDB - Server:167.99.10.46 DB:${database}`))
+            .catch(err => console.log('error :::: ' + err));
+    },
+};
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express'); // remember to install your npm packages
-
 const server = express();
+const db = require('./db');
 
 // add your server code
 
+db.connectTo('btracker')
+    .then(() => console.log('\n... API Connected to Database ...\n'))
+    .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`Server up and running on ${port}`);
+    console.log(`Server up and running on ${port}`);
 });
