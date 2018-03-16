627dc5351bed78d6fc2c603eb6843436a462790e
aceredesign <steven.magadan@hotmail.com>
2018-03-16 10:50:29 -0700

    added categories

--- server.js
+++ server.js
@@ -49,6 +49,35 @@ server.get("/budget", (req, res) => {
         .json({ message: "There was an error getting your entries" });
     });
 });
+
+server.post("/category", (req, res) => {
+  const { title } = req.body;
+  if (title) {
+    const category = new Category(req.body);
+    category
+      .save()
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Could not post category" });
+      });
+  } else {
+    res.status(400).json({ error: "You need to put a title for the category" });
+  }
+});
+
+server.get("/category", (req, res) => {
+  Category.find()
+    .select({ title: 1, _id: 0 })
+    .then(results => {
+      res.status(200).json(results);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not get categories" });
+    });
+});
+
 mongoose.connect("mongodb://localhost/budget", { useMongoClient: true });
 
 const port = process.env.PORT || 8000;
