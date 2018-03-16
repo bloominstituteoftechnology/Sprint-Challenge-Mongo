8ebd4ff5d6292cd470a767cca1bbfdf2306e1922
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-16 12:44:59 -0500

    POST Route for Budget
    in budgetRouter.js:
    add logic for budget post
    require title and budgetAmount
    create new documnet in collection
    save document
    return new object
    catch errors
    in budget.js:
    change type of budgetAmount to Number

--- Budget/budget.js
+++ Budget/budget.js
@@ -3,7 +3,7 @@ const objectID = mongoose.Schema.Types.ObjectId;
 
 const Budget = new mongoose.Schema({
   title: { type: String, required: true },
-  budgetAmount: { type: String, required: true }
+  budgetAmount: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Budget', Budget);
