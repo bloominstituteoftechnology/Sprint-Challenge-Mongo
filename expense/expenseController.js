eb2315ada7349cd33eae62681f4b8559da67e8a4
Arkoma <aaron@adburk.net>
2018-03-16 14:09:22 -0500

    continuing wip with populating budgets and categories

--- expense/expenseController.js
+++ expense/expenseController.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const ExpenseRouter = express.Router();
-const ExpenseModel = require('./ExpenseModel');
+const Expense = require('./ExpenseModel');
+const Budget = require('../budget/budgetModel');
+// const Category = require('../category/categoryModel');
 
+ExpenseRouter.post('/', (req,res) => {
+  const ExpenseInfo = req.body;
+  const newExpense = new Expense(ExpenseInfo);
+  newExpense
+    .save()
+    .then(savedExpense => {
+      res.status(200).json(savedExpense);
+    })
+    .catch(err => {
+      res.status(500).json({error: 'There was an error saving your expense to the database'})
+    })
+})
 
+ExpenseRouter.get('/', (req,res) => {
+  Expense.find({})
+    .populate('Budget')
+    .populate('Category')
+    .then(expenses => {
+      console.log('expenses', expenses);
+      Budget.find({})
+        .then(budgets => {
+          console.log('budgets: ', budgets);
+          console.log(expenses._doc);
+          const expense = {...expenses, budget: budgets};
+          res.send(expense);
+        })
+      // res.status(200).json(expenses);
+    })
+    .catch(err => {
+      res.status(500).json({error: 'There was an error retrieving your expenses from the database'})
+    })
+});
 
 module.exports = ExpenseRouter;
