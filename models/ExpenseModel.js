c9589093352310edd92e6b1cf7cd2b0a3d7f2017
Christopher Beards <beardscm@gmail.com>
2018-03-16 10:41:26 -0400

    Created ExpenseModel and Imported Related Models

---
+++ models/ExpenseModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+require('./BudgetModel.js')
+require('./CatgegoryModel.js')
+
+
+const ExpenseSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  budget: { type: ObjectId, ref: 'Budget' },
+  category: { type: ObjectId, ref: 'Category' },
+});
+
+module.exports = mongoose.model('Expense', ExpenseSchema);
