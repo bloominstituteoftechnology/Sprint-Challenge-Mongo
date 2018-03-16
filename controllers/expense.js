a4e44cc6b762f4fe615f7b0f3fb4e7ac854287f4
temp <temp@Debis-Home-Mac.local>
2018-03-16 13:56:53 -0400

    Made expense posts subtract the expense from the budget. This was an addition outside of the main assignment.

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const Expense = require('../models/expense');
+const Budget = require('../models/budget');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   const info = req.body;
+  const id = info.budget;
+
   if (info) {
     expense = new Expense(info);
     expense
       .save()
       .then(newExpense => {
-        res.status(201).send(newExpense);
+        Budget.findById(id)
+          .then(budget => {
+            budget.budgetAmount -= newExpense.amount;
+            budget.save();
+          })
+          .then(() => {
+            Expense.find()
+              .select('-_id -__v')
+              .populate('budget', '-_id -__v')
+              .populate('category', '-_id -__v')
+              .then(expenses => {
+                res.status(201).send({ newExpense, allExpenses: expenses });
+              });
+          });
       })
       .catch(err => {
         res.status(500).send({ error: err });
