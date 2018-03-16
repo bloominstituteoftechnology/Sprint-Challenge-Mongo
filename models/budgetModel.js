9ceb5b23ae884c95c9ba828a692de0ad2d56ad77
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-16 11:15:02 -0600

    Add title and budget amount properties to Budget schema

--- models/budgetModel.js
+++ models/budgetModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const Budget = new mongoose.Schema({
-  
+  title: String,
+  budgetAmount: Number,
 });
 
 module.exorts = mongoose.model('Budget', Budget);
