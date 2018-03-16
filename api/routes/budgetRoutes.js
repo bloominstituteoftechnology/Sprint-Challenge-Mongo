60a9f5d0fe5f9a711ec637fc4684138daccf5f78
german gonzalez <gcomlnk@gmail.com>
2018-03-16 15:00:47 -0400

    creating endpoint to insert budget info

--- api/routes/budgetRoutes.js
+++ api/routes/budgetRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const budgetController = require('../controllers/budgetController');
 let budgetRouter = express.Router();
 
+// EndPoint: /api/budget/
+
+// get all the budgets saved.
 budgetRouter.route('/').get(budgetController.getBudget);
+
+// Insert an amount on budget collection.
+budgetRouter.route('/').post(budgetController.insertBudget);
 
 module.exports = budgetRouter;
