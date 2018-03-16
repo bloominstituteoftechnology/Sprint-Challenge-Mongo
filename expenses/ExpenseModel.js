8bbf991a9f9b0098b8573a721a324f2982e23b9d
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-16 11:50:26 -0400

    Expense Model Finished
    ExpenseModel.js --- Finished schema/model for Expense documents.
    
    ExpenseRouter.js --- Empty, going to write Category model first.

---
+++ expenses/ExpenseModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const ExpenseSchema = new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    budget: {
+        type: ObjectId,
+        ref: 'Budget'
+    },
+    category: {
+        type: ObjectId,
+        ref: 'Category'
+    },
+})
+
+const ExpenseModel = mongoose.model('Expense', ExpenseSchema);
+
+module.exports = ExpenseModel;
