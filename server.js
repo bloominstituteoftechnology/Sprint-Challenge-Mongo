2c6989e08fa91a9113c5b37e90adb73b5629779f
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-16 11:36:22 -0600

    Setup initial server
    Check if server running
    
    Server successfuly up and running

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
 
 const server = express();
+server.use(express.json());
+server.use(helmet());
 
 // add your server code
+
+server.get('/', (req, res) => res.send('API Running...'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
