15b57a449411ce255fecfd4956054b957f50023f
Terrie B. Kim <terrieb.kim@gmail.com>
2018-03-16 08:43:40 -0700

    server setup complete

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express'); // remember to install your npm packages
-
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 const server = express();
 
 const Budget = require('./Budget/BudgetModel.js');
 const Category = require('./Category/CategoryModel.js');
 const Expense = require('./Expense/ExpenseModel.js');
 
-// add your server code
 mongoose.connect('mongodb://localhost/budget-tracker');
 
+server.use(cors());
 
+// add your server code
 
-const port = process.env.PORT || 5000;
+server.post('/budget', (req, res) => {
+
+});
+
+server.get('/', (req, res) => res.send('API Running...'));
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
