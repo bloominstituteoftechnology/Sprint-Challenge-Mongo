1055de23b904245ac3addef4f3b6dea011d6758f
Terrie B. Kim <terrieb.kim@gmail.com>
2018-03-16 08:35:28 -0700

    model/schemas complete

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express'); // remember to install your npm packages
 
 const server = express();
 
+const Budget = require('./Budget/BudgetModel.js');
+const Category = require('./Category/CategoryModel.js');
+const Expense = require('./Expense/ExpenseModel.js');
+
 // add your server code
+mongoose.connect('mongodb://localhost/budget-tracker');
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
