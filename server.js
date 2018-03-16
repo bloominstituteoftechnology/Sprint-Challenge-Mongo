55c23ea68a63791c16c8255caf8900c7b9eee252
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-16 10:54:50 -0500

    add missing requirements and posting to /budget
    - lines 4-5: import helmet and mongoose
    
    - lines 15-25: POST to /budget endpoint
    
    - lines 27-30: connect to MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const server = express();
+
+const Budget = require('./models/Budget.js');
 
 server.use(cors());
 server.use(helmet());
 server.use(bodyParser.json());
+
+server.post('/budget', (req, res) => {
+  const budget = new Budget(req.body);
+  budget
+    .save()
+    .then(budget => {
+      res.json(budget);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+mongoose
+  .connect('mongodb://localhost/budgets')
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.log('Connection to MongoDB failed'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
