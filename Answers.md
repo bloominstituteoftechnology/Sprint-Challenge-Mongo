e06dcf26387d5bb51b46aa73a3742d52001af634
Michael Marshal <mikemarshalman@gmail.com>
2018-03-16 11:54:29 -0500

    budget model + router

--- Answers.md
+++ Answers.md
@@ -1,8 +1,3 @@
 # 1) A database is a structured set of data in a computer that can be accessed in various ways. The document a the unit of storing data (uses JSON for storing data). A collection can store many documents.
 # 2) We use populate to get different documents in the database and link them in the schemas. ObjectID and ref are used to point to different documents, which are then populated.
 #
-#
-#
-#
-#
-#
---
+++ budget/budgetRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const Budget = require('./budget.js');
+
+const budgetRouter = express.Router();
+
+budgetRouter.post('/', (req, res) => {
+  const info = req.body;
+  const budget = new Budget(info);
+
+  budget
+    .save()
+    .then(saveBud => {
+      res
+        .status(200)
+        .json(saveBud);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ MESSAGE: 'Budget saving error', error: err });
+    });
+});
+
+module.exports = budgetRouter;
