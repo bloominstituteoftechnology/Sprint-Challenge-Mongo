b2dc435e4a99426ab4930554bb6b090c58a7f2f5
Christopher Beards <beardscm@gmail.com>
2018-03-16 10:29:39 -0400

    Server Running

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express'); // remember to install your npm packages
-
+const express = require('express'); 
 const server = express();
+const mongoose = require('mongoose');
 
-// add your server code
+//==============================
+//      SERVER INFORMATION
+//==============================
+
+mongoose
+  .connect('mongodb://localhost/budget')
+  .then(() => {
+    server.listen(3000, () => console.log('API Server running on port 3000'));
+  })
+  .catch(error => {
+    console.error('database connection failed');
+  });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
