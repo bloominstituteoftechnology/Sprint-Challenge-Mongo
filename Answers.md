085073dc3445e4a855147aba9e36143c08487a0d
johnjohnx4 <johnjohnx4@gmmail.com>
2018-03-16 15:16:02 -0400

    first commit, started late

---
+++ Answers.md
@@ -0,0 +1,13 @@
+1.  Describe the following: `DataBase`, `Collection` , `Document`
+- Database: something that holds and stores data/ information
+- Collection: a stored series of of data
+- Document: Data saved in the database
+
+So a related data are all documents stored in a collection which exists in a database.
+
+2.  Describe how one can achieve the pattern of _relationships_ in MongoDB. What needs to take place at the schema level? How do we _'fill'_ in the appropriate relational data using mongoose?
+
+
+
+3.  Explain a way to break up an API into Sub-Applications, which tool did we use to do that?
+- Use middleware, such as Express Routers
--- server.js
+++ server.js
@@ -1,10 +1,57 @@
-const express = require('express'); // remember to install your npm packages
-
+const express = require('express');
 const server = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Budget = require('./api/models/budgetModel');
+const Category = require('./api/models/categoryModel');
+const Expense = require('./api/models/expenseModel');
 
 // add your server code
 
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/budget');
+
+const db = mongoose.connect('mongodb://localhost/budget', {
+  useMongoClient: true
+});
+
+server.use(bodyParser.json());
+
+server.post('/budget', (req, res) => {
+  const title = req.body.title
+  const budgetAmount = req.body.budgetAmount;
+
+  if (!title || !budgetAmount) res.send({ error: 'You are missing either a title or budget amount!' });
+
+  const budget = new Budget(req.body);
+  Budget.create(budget)
+  .then(data => { res.send(data) })
+  .catch(err => { res.send(err) });
+})
+
+server.post('/category', (req, res) => {
+  const title = req.body.title
+
+  if (!title) res.send({ error: 'You are missing a title!' });
+
+  const category = new Category(req.body);
+  Category.create(category)
+  .then(data => { res.send(data) })
+  .catch(err => { res.send(err) });
+})
+
+server.get('/category', (req, res) => {
+  Category.find()
+  .select("title")
+  .then(titles => {res.json(titles);})
+  .catch(err => { res.send(err) });
+})
+
+
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
