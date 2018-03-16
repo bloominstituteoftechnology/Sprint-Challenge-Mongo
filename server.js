538cf62a6eafadb0d41df1ff73f75d4bbf908df4
Charlie Sparks <csparks@antioch.edu>
2018-03-16 12:18:29 -0700

    expense model finished and working; population functional as well for both key/value pairs inside of the expense document

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 //import models
 const BudgetModel = require("./models/Budget");
 const CategoryModel = require("./models/Category");
+const ExpenseModel = require("./models/Expense");
 
 //starting mongoose and express
 mongoose.connect("mongodb://localhost:27017/sprint")
@@ -36,4 +37,20 @@ mongoose.connect("mongodb://localhost:27017/sprint")
 // .catch(err => console.log(`There was an error: \n ${err}`))
 
 //creating and saving the expense document
+// const newExpense = new ExpenseModel({
+//   amount: 20,
+//   description: "Switch Games",
+//   budget: mongoose.Types.ObjectId("5aac12951cc99d05ac436313"),
+//   category: mongoose.Types.ObjectId("5aac14fe8c245b0948668a47"),
+// })
+// .save()
+// .then(response => console.log(`The expense was saved: \n ${response}`))
+// .catch(err => console.log(`There was an error: \n ${err}`));
 
+//populating the expense document
+// ExpenseModel
+// .find()
+// .populate("budget")
+// .populate("category")
+// .then(response => console.log(response))
+// .catch(err => console.log(err));
