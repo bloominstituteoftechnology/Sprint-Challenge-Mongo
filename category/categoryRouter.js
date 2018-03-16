9f47f4aa16fcb2312b804f54a4c34558cb7d9e5c
Smithface <kylewibblesmith@gmail.com>
2018-03-16 12:07:13 -0400

    wrote category router

---
+++ category/categoryRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const Category = require('./categoryModel');
+
+const router = express.Router();
+
+router.post('/', function(req, res) {
+  const categoryInfo = req.body;
+  const newCategory = new Category(categoryInfo);
+
+  newCategory
+    .save()
+    .then(category => {
+      res.status(201).json(category);
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'error creating category', error: err });
+    });
+});
+
+router.get('/', function(req, res) {
+  Category
+    .find()
+    .then(categories => {
+      res.status(200).json(categories);
+    })
+    .catch(
+      res.status(500).json({ msg: 'error getting categories', error: err })
+    );
+});
+
+module.exports = router;
