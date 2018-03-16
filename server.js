a6311cf8a4d69d233559e9e6f540c090a3c25af7
algorithmsrock <algorithmsrock@gmail.com>
2018-03-16 13:37:05 -0700

    Connected server.js

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express'); // remember to install your npm packages
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
 
+const CategoryRouter = require('../Category/CategoryRouter.js');
+const BudgetRouter = require('../Budget/BudgetRouter.js');
+const ExpenseRouter = require(../Expense/ExpenseRouter.js);
 const server = express();
 
-// add your server code
+server.use(helmet());
+server.use(bodyParser.json());
+
+mongoose
+  .connect('mongob://localhost/Budget Tracker')
+	.then(() => console.log('Successfully connected to Budget Tracker Database!'))
+  .catch(err => console.log('Error connecting to Budget Tracker Database'));
+
+server.use('/Budget', BudgetRouter);
+server.use('/Expense', ExpenseRouter);
+server.use('/Category', CategoryRouter);
+
+server.get('/', (req, res) => res.send('API running...'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
