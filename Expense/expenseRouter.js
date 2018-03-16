dcca11da757d60ad1291d429a069a6360a86ae78
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-16 13:58:58 -0500

    Expense Routes
    in expenserRouter.js:
    add GET function
    retrieve all expenses
    populate with budget and category
    return all expenses to user
    catch errors
    add POST method
    require amount and description
    save the post
    assign budget and category based on relationship/_id
    handle errors
    incomplete

--- Expense/expenseRouter.js
+++ Expense/expenseRouter.js
@@ -4,7 +4,38 @@ const Expense = require('./expense');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json({ status: 'Your Expense shit is connected' });
+  Expense.find({})
+    .populate('budget', 'title budgetAmount')
+    .populate('category', 'title')
+    .then(expenses => {
+      res.status(200).json( { expenses: expenses });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The information could not be retrieved' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const { amount, description, budget, category } = req.body;
+  if (!amount || !description) {
+    res.status(400).json({ errorMessage: 'Please provide an amount and description in the request body' });
+  }
+  const expense = new Expense({
+    budget: budget._id,
+    category: category._id,
+    amount,
+    description,
+  });
+  expense.save()
+    .then(newExpense => {
+      res.status(201).json(newExpense);
+    })
+    .catch(err => {
+      if (err) {
+        res.status(400).json({ errorMessage: 'there was a user error', errorBody: err });
+      }
+      res.status(500).json({ errorMessage: 'There was an error while saving the expense to the database', err});
+    });
 });
 
 module.exports = router;
