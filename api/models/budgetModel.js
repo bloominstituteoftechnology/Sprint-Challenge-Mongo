2bfe794cc83bf8253b53d390b7d42710ba5fa2a6
german gonzalez <gcomlnk@gmail.com>
2018-03-16 14:41:59 -0400

    Creating budget model

---
+++ api/models/budgetModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+// const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const BudgetSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please provide title.'],
+    },
+    budgetAmount: {
+        type: String,
+    },
+    createdOn: {
+        type: String,
+        required: true,
+        default: new Date(),
+    }
+});
+
+module.exports = mongoose.model('budgets', BudgetSchema);
