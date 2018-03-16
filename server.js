593273fa23a4b438ee9ba40ffebd5494e25e52f5
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-16 12:04:55 -0600

    Setup API routes in server

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ const helmet = require('helmet');
 
 const server = express();
 
+const db = require('./data/db');
+
+const budgetRouter = require('./routers/budgetRouter');
+const expenseRouter = require('./routers/expenseRouter');
+const categoryRouter = require('./routers/categoryRouter');
+
+db
+.connectTo('budgetTracker')
+.then(() => console.log('\n... API Connected to Database ...\n'))
+.catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+
+// add your server code
 server.use(express.json());
 server.use(helmet());
 
-const db = require('./data/db');
-
-db
-  .connectTo('budgetTracker')
-  .then(() => console.log('\n... API Connected to Database ...\n'))
-  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
-
-// add your server code
+server.use('api/budget', budgetRouter);
+server.use('api/expense', expenseRouter);
+server.use('api/category', categoryRouter);
 
 server.get('/', (req, res) => res.send('API Running...'));
 
