cf54681363d58cd8a2ec1c7644095fd3de5b9f86
AJ Genung <DragonRydrz@me.com>
2018-04-13 11:20:37 -0400

    server.js built.  Collection folders created

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/Mongo-Sprint')
+  .then(() => '\n\n===Connected to MongoDB===\n\n')
+  .catch(() => 'There was an issue connecting to the DataBase.');
 
 const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+const budgetRouter = require('./budget/budgetRouter');
+const categoryRouter = require('./category/categoryRouter');
+const expenseRouter = require('./expense/expenseRouter');
+
+server.use('/api/budget', budgetRouter);
+server.use('/api/category', categoryRouter);
+server.use('/api/expense', expenseRouter);
 
 // add your server code
 
