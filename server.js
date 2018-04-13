aa7dd7e8c0a8010bbd2923e621079bf4bac12844
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-13 12:23:10 -0400

    almost completed server.js

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const budgetRouter = require('./budget/budgetRouter.js');
+const categoryRouter = require('./category/categoryRouter.js');
+const expenseRouter = require('./expense/expenseRouter.js');
+
+mongoose
+  .connect('mongodb://localhost/5000')
+  .then(() => console.log('\n... API dancing with Database ...\n\n'))
+  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n\n', err));
 
 const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use('/api/budget', budgetRouter);
+// server.use('/api/category', categoryRouter);
+// server.use('/api/expense', expenseRouter);
+
+// routes will go here
 
 // add your server code
 
