27ed6fdb5ff431e1cad5a7cf35853fea1429b89c
nourmhassan <nour-pop1@hotmail.com>
2018-04-13 10:12:33 -0700

    files created, server and budget created

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-mongoose
-  .connect('mongodb://localhost/Mongo-Sprint')
-  .then(() => '\n\n===Connected to MongoDB===\n\n')
-  .catch(() => 'There was an issue connecting to the DataBase.');
+mongoose.connect('mongodb://localhost/budgettrackerdb')
+.then(() => console.log('\n=== connected to mongo ===\n'))
+.catch(error => console.log('There was an error connecting to mongo. :('));
 
+const budgetController = require('./budget/budgetController');
+const expenseController = require('./expense/expenseController');
+const categoryController = require('./category/categoryController')
 
-  const server = express();
-
-// add your server code
+const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.status(500).json({ api: 'running'});
+});
 
+server.use('/api/budgets', budgetController);
+ 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
