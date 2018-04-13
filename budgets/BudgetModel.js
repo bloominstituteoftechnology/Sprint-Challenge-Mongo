c5d7d92c6ba18bd9dd5fc4662f95265150f6575f
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-04-13 15:10:17 -0700

    validation added

--- budgets/BudgetModel.js
+++ budgets/BudgetModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const Budget = mongoose.Schema({
-  // _id: ObjectId('507f1f77bcf86cd799439011'),
   title: String,
   budgetAmount: Number,
 });
--- expenses/expensesRouter.js
+++ expenses/expensesRouter.js
@@ -4,7 +4,11 @@ const Expense = require('./ExpenseModel.js');
 
 router.get('/', async (req, res) => {
   try {
-    const expenses = await Expense.find({});
+    const expenses = await Expense.find({}).populate('budget category', {
+      _id: 0,
+      title: 1,
+      budgetAmount: 1,
+    });
     res.status(200).json(expenses);
   } catch (err) {
     res.status(500).json(err);
