4c0c557d475f65759145817d34e7b5e29b068231
nourmhassan <nour-pop1@hotmail.com>
2018-04-13 09:20:52 -0700

    server up

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const server = express();
+mongoose
+  .connect('mongodb://localhost/Mongo-Sprint')
+  .then(() => '\n\n===Connected to MongoDB===\n\n')
+  .catch(() => 'There was an issue connecting to the DataBase.');
+
+
+  const server = express();
 
 // add your server code
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
