fe9f4f69f291e5af0fbce6ab3329c0cd2e2b970e
RiaClay <sherria.rydyn@gmail.com>
2018-04-13 11:20:10 -0500

    created get and post router for categories.

--- budgets/budgetController.js
+++ budgets/budgetController.js
@@ -7,14 +7,14 @@ router
     .post((req, res) => {
         const budget = new Budget(req.body);
 
-        Budget
+        budget
             .save()
             .then(newBudget => {
-                res.status(200).json(newBudget);
+                res.status(201).json(newBudget);
             })
             .catch(err => {
                 res.status(500).json({ errMsg: "Could not save new budget information." })
             })
     })
-    
+
 module.exports = router;
