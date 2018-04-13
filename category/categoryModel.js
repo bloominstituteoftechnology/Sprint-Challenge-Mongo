d94de1b26af01364182c169cfadd0a77f0b4e1ec
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-13 12:54:33 -0400

    completed categoryModel.js

--- category/categoryModel.js
+++ category/categoryModel.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const category = new mongoose.Schema({
+  _id: { type: ObjectId },
+  title: { type: String, required: true, unique: true }
+});
+const categoryModel = mongoose.model('Category', categorySchema);
+module.exports = categoryModel;
