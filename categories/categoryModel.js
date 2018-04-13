b563531ba6c1b43dc50e438b0174a500d3175904
RiaClay <sherria.rydyn@gmail.com>
2018-04-13 11:04:07 -0500

    Finished setting up the schemas for budgets, expenses,and categories.

--- categories/categoryModel.js
+++ categories/categoryModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema ({
 
+const categorySchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        required: true
+    }
 })
 
 const categoryModel = mongoose.model('Category', categorySchema);
--- expenses/expenseModel.js
+++ expenses/expenseModel.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Budget = require('../budgets/budgetModel');
+const Category = require('../categories/categoryModel');
+
 
 const expenseSchema = new mongoose.Schema ({
+    amount: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
 
+    },
+    budget: {
+        type: ObjectId,
+        ref: 'Budget'
+    },
+    category: {
+        type: ObjectId,
+        ref: 'Category'
+    }
 })
 
 const expenseModel = mongoose.model('Expense', expenseSchema);
