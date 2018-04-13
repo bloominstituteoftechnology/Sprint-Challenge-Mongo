f99a18487fd80d284aac2e552fa2a38fa63c0ca3
Edward Manda <lilkups@yahoo.com>
2018-04-13 11:02:56 -0500

    Edward Manda - Sprint Challenge Mongo, Answers done!  express, morgan, helmet, cors, mongoose installed. config file setup. server.js rigged up.

---
+++ Answers.md
@@ -0,0 +1,13 @@
+#1
+-- Database: a collection of information for later retrieval
+-- Collection: is an object containing all the information about a certain topic
+-- Document is a piece of information in a Collection
+
+#2
+-- one to one
+-- one to many
+-- one to a few
+-- many to many
+
+#3
+-- Use express().Router() and sub folders
--- server.js
+++ server.js
@@ -1,10 +1,43 @@
-const express = require('express'); // remember to install your npm packages
+// bring in express, morgan, helmet, cors and mongoose
+const express = require(`express`);
+const morgan = require(`morgan`);
+const helmet = require(`helmet`);
+const cors = require(`helmet`);
+const mongoose = require(`mongoose`);
 
+// bring in the config file;
+const config = require('./api/config.js');
+
+// create a server
 const server = express();
 
-// add your server code
+// to enable json parsing
+server.use(express.json());
 
-const port = process.env.PORT || 5000;
+//logging, TBD: create morganOptions and pass it in, instead
+server.use(morgan(`dev`));
+
+//security
+server.use(helmet());
+
+//cross origin request sharing permissions
+const corsOptions = {
+  origin: `*`,
+  methods: `GET, HEAD, PUT, PATCH, POST, DELETE`,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+server.use(cors(corsOptions));
+
+// root get request
+server.get(`/`, (req, res) =>
+  // send a json response
+  res.json({ api: `API is running successfully!!` })
+);
+
+// define a port to use
+const port = config.port || 5000;
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
