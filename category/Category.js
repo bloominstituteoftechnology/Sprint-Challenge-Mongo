018b18b45f525c704119cc8d57c8e4136baaaf9c
AJ Genung <DragonRydrz@me.com>
2018-04-13 12:36:59 -0400

    added required and unique to the schema

--- category/Category.js
+++ category/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  title: String,
+  title: { type: String, required: true, unique: true },
 });
 
-module.exports = mongoose.model('Category'.categorySchema);
+module.exports = mongoose.model('Category', categorySchema);
