78d731bf41dae9c72ccd6db3418917c9513ace26
noxasaxon <saxon.h@outlook.com>
2018-04-13 13:37:49 -0700

    server dine

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import { SSL_OP_CIPHER_SERVER_PREFERENCE } from 'constants';
-
 const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
 const cors = require('cors');
@@ -10,10 +8,18 @@ const server = express();
 // add your server code
 const budgetRouter = require('./Budget/BudgetRouter');
 
+mongoose
+  .connect('mongodb://localhost/finance')
+  .then(() => console.log('\n=== connected to Mongo ===\n'))
+  .catch(err => console.log('error connecting to DB'));
+
 server.use(helmet);
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/budgets', budgetRouter);
+
+server.get('/', (req, res) => res.send('API running...'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
