a554196d36ad6440944f7442b69eab0434caae03
Megan Williamson <meganawilliamson@gmail.com>
2018-04-13 08:28:15 -0700

    server running

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+// Express
 const express = require('express'); // remember to install your npm packages
+// Mongoose
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb://localhost/budgetdb')
+  .then(() => console.log('\n=== Connected to Mongo === \n'))
+  .catch(() => console.log('Error connecting to Mongo'));
 
+// Controller
+const budgetsController = require('./budgets/Budget');
+const categoriesController = require('./categories/Category');
+const expensesController = require('./expenses/Expense');
+
+// Server
 const server = express();
 
-// add your server code
+// Middleware
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'Running...' });
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
