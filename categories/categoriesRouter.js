d3acd6161aed0451e2849060da040e77fe04474c
Justin Kaseman <justinkaseman@live.com>
2018-04-13 10:51:38 -0700

    Add findById

--- categories/categoriesRouter.js
+++ categories/categoriesRouter.js
@@ -7,14 +7,20 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	Category.find({})
 		.select('title')
-		.then(budgets => res.status(200).json(budgets))
+		.then(categories => res.status(200).json(categories))
+		.catch(error => res.status(500).json(`Error from server: ${error}`));
+});
+
+router.get('/:id', (req, res) => {
+	Category.findById(req.params.id)
+		.then(category => res.status(200).json(category))
 		.catch(error => res.status(500).json(`Error from server: ${error}`));
 });
 
 router.post('/', (req, res) => {
 	const category = req.body;
 	Category.create(category)
-		.then(budget => res.status(201).json('Saved category'))
+		.then(category => res.status(201).json(`Saved: ${category}`))
 		.catch(error => res.status(500).json(`Error from server: ${error}`));
 });
 
--- expenses/expensesRouter.js
+++ expenses/expensesRouter.js
@@ -12,10 +12,22 @@ router.get('/', (req, res) => {
 		.catch(error => res.status(500).json(`Error from server: ${error}`));
 });
 
+router.get('/:id', (req, res) => {
+	Expense.findById(req.params.id)
+		.then(expense => res.status(200).json(expense))
+		.catch(error => res.status(500).json(`Error from server: ${error}`));
+});
+
 router.post('/', (req, res) => {
 	const expense = req.body;
 	Expense.create(expense)
-		.then(budget => res.status(201).json('Saved expense'))
+		.then(exp => res.status(201).json(`Saved: ${exp}`))
+		.catch(error => res.status(500).json(`Error from server: ${error}`));
+});
+
+router.delete('/:id', (req, res) => {
+	Expense.findByIdAndRemove(req.params.id)
+		.then(expense => res.status(200).json(expense))
 		.catch(error => res.status(500).json(`Error from server: ${error}`));
 });
 
