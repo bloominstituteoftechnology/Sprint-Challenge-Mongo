12b76db1f5b2f851adbaa1a37f6b1545d58a6f90
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-13 10:22:40 -0700

    error checking and budget, category post implemented

--- budgets/budgetsRouter.js
+++ budgets/budgetsRouter.js
@@ -14,4 +14,27 @@ router.route('/').get((req, res) => {
     });
 });
 
+router.route('/').post((req, res) => {
+  const budget = new Budget(req.body);
+
+  if (!req.body.title || !req.body.budgetAmount) {
+    res.status(400).json({
+      errorMesage:
+        'Budget title (string) and budgetAmount (number) fields are required.',
+    });
+    return;
+  }
+
+  budget
+    .save()
+    .then(savedBudget => {
+      res.status(201).json(savedBudget);
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: 'There was an error saving the budget to the database',
+      });
+    });
+});
+
 module.exports = router;
--- categories/categoriesRouter.js
+++ categories/categoriesRouter.js
@@ -6,11 +6,33 @@ const router = express.Router();
 
 router.route('/').get((req, res) => {
   Category.find({})
+    .select({ title: 1, _id: 0 })
     .then(categories => {
       res.status(200).json(categories);
     })
     .catch(err => {
       res.status(500).json(err);
+    });
+});
+
+router.route('/').post((req, res) => {
+  const category = new Category(req.body);
+
+  if (!req.body.title) {
+    res.status(400).json({ errorMessage: 'Title field required (string).' });
+    return;
+  }
+
+  category
+    .save()
+    .then(savedCategory => {
+      res.status(201).json(savedCategory);
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage:
+          'There was an error while saving the category to the database.',
+      });
     });
 });
 
