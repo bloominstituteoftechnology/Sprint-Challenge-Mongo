082a92d27f78d054cccd48cda2747c3344af5de8
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-13 08:34:07 -0700

    server up; file structure implemented

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express'); // remember to install your npm packages
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+mongoose
+  .connect('mongodb://localhost/budgettracker')
+  .then(() => console.log(`\n=== connected to mongo ===\n`))
+  .catch(err => res.status(500).json(err));
 
 const server = express();
 
 // add your server code
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running' });
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
