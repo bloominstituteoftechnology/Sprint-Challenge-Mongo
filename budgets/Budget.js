94cd8fb2977e73818a30c20dbe359fd284c982f0
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-04-20 18:44:56 -0400

    post to budgets working

--- budgets/Budget.js
+++ budgets/Budget.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const Budget = new Schema({
+const Budget = mongoose.Schema({
 	title: { type: String, required: true },
 	budgetAmount: { type: Number, required: true },
 });
--- categories/categoriesRouter.js
+++ categories/categoriesRouter.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 //endpoints
 
+
+
 module.exports = router;
