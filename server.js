1013f8696072dece7c9385e0e1041b30ec5cd477
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-11 11:02:17 -0400

    server is running

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express'); // remember to install your npm packages
-
+const helmet = require('helmet');
 const server = express();
 
-// add your server code
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => res.send("Server is running."))
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
