42c6f73c4c27f9eeb6ddc660892ab67fafb2f582
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-11 08:43:14 -0700

    Built out schemas
    finished up models and moving onto the end points

---
+++ Expense/expense.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Expense = new mongoose.Schema({
+amount:{type:Number, required:true},
+description:String,
+budget:{type:ObjectId,ref:'Budget'},
+category:{type:ObjectId,ref:'Category'}
+
+
+
+
+})
+module.exports = mongoose.model(Expense, Expense)
