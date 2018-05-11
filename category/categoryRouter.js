dacd31b910a021f8cf7f58b86de2054f31bdcfa7
cgoez <goez2001@gmail.com>
2018-05-11 10:15:57 -0700

    categoryRouter POST /

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -20,4 +20,28 @@ router.route("/").get((req, res) => {
     });
 });
 
+// POST /
+router.route("/").post((req, res) => {
+  const { title } = req.body;
+  const category = new Category(req.body);
+
+  // Make sure title provided
+  if (title) {
+    category
+      .save()
+      .then(category => {
+        res.status(201).json(category);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error posting the new category."
+        });
+      });
+  } else {
+    res.status(404).json({
+      error: "Please provide a TITLE to continue."
+    });
+  }
+});
+
 module.exports = router;
