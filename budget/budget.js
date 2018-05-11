2e71c12ccf2d287c009ac80ef4ea78593a3bf39b
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-11 09:16:24 -0700

    Initial setup of Budget model files

---
+++ budget/budget.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Budget = mongoose.Schema({
+//data here
+}); 
+
+
+
+
+
+//module export
+module.exports = mongoose.model('Budget', Budget);
---
+++ budget/budgetRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Budget = require('./Budget.js');
+
+//express router
+const router = express.Router();
+//set end points here
+
+
+
+
+
+//module export
+module.exports = router;
