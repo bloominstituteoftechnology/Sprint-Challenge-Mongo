99972e272bb1d1f55be0479d632cb313ebc92cc3
Mark Wong <fmarkwong@gmail.com>
2018-05-11 10:14:47 -0700

    answers and mongoose setup

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express'); // remember to install your npm packages
 
 const server = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/budgets_db')
+  .then(mongo => console.log('connected to database'))
+  .catch(err => console.log(err));
+
+
+server.use(express.json());
 
 // add your server code
 
