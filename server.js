a6dd1843adefecbf205982cb48e5dd1cbb57036d
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-11 13:18:40 -0500

    set-up server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express'); // remember to install your npm packages
 
+const morgan = require('morgan');
+const helmet = require('helmet');
 const server = express();
 
 // add your server code
+const db = require('./data/db.js');
+const budgetRouter = require('./budget/budgetRouter');
+const expenseRouter = require('./expense/expenseRouter');
+const categoryRouter = require('./Category/CategoryRouter');
+
+db
+  .connectTo('budgetTracker')
+  .then(() => console.log('\n... API connected to Database ...\n'))
+  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err))
+
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(express.json())
+
+server.use('/budget', budgetRouter);
+server.use('/expense', expenseRouter);
+server.use('/category', categoryRouter);
+
+server.get('/', (req, res) => res.send('API Running'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
