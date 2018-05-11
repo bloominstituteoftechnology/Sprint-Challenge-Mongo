24c62c9dcdce4d2be4d363f41cf61109b24762fe
Jared Cuffe <jcuffe@gmail.com>
2018-05-11 07:44:33 -0700

    Error objects for use in server.js

---
+++ src/errors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  budgetTitle: {
+    status: 400,
+    message: 'You must include a title when creating a budget'
+  },
+  budgetAmount: {
+    status: 400,
+    message: 'You must include a budgetAmount when creating a budget'
+  },
+  server: {
+    status: 500,
+    message: 'Internal server error'
+  }
+}
