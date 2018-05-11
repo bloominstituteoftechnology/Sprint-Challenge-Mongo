6c29c2d36d9818c99f7296371c8e2e26c42a0364
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-11 11:48:16 -0400

    sike - apparently we don't want a get for the budget router

--- budget/budgetController.js
+++ budget/budgetController.js
@@ -3,12 +3,13 @@ const Budget = require('./budgetModel');
 const router = express.Router();
 
 router.route('/')
-  .get((req, res) => {
-    Budget
-      .find()
-      .then(budgets => res.status(200).json(budgets))
-      .catch(err => res.status(500).json({ error: "There was an error fetching your budgets." }))
-  })
+  .get((req, res) => res.send("Budget router is functional."))
+  // .get((req, res) => {
+  //   Budget
+  //     .find()
+  //     .then(budgets => res.status(200).json(budgets))
+  //     .catch(err => res.status(500).json({ error: "There was an error fetching your budgets." }))
+  // })
 
   .post((req, res) => {
     const { title, budget } = req.body;
