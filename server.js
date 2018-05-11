503b9e778704c6a18fb6975ce5ab9ee6774aa5b6
Harrison Brock <harrison@harrisonbrock.com>
2018-05-11 11:20:44 -0400

    db.js -- Added helper function to connect to any MongoDB server.js -- Connect to database and setup default route

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express'); // remember to install your npm packages
+const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
 
+// api require
+const db = require('./data/db');
+
+// connect to database
+db.conntectTo('budgetTracker')
+    .then(() => console.log('\n... API Connected to Database ...\n'))
+    .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+
 // add your server code
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+  res.status(201).json('Server is running')
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
