81eb294ce46de2596463a1e046dccae064df206a
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-11 09:01:24 -0700

    added budgetRouter functionality

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
 
 const budgetRouter = require('./budget/budgetRouter.js');
-const expenseRouter = require('./expense/expenseRouter.js');
-const categoryRouter = require('./category/categoryRouter.js');
+//const expenseRouter = require('./expense/expenseRouter.js');
+//const categoryRouter = require('./category/categoryRouter.js');
 
 const server = express();
 
@@ -12,8 +12,8 @@ server.use(helmet());
 server.use(express.json());
 
 server.use('./api/budgets', budgetRouter);
-server.use('./api/expenses', expenseRouter);
-server.use('./api/categories', categoryRouter);
+//server.use('./api/expenses', expenseRouter);
+//server.use('./api/categories', categoryRouter);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
