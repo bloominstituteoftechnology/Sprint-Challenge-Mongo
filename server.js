83d7eb837e38e0249068e57777584695303170f7
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-05-11 12:55:23 -0400

    boilerplate / setup server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+
+mongoose
+  .connect('mongodb://localhost/savemoneys')
+  .then(() => console.log(' > API dancing with Database '))
+  .catch(err => console.log('** :( ERROR Connecting to Database **', err));
 
 const server = express();
+
+
+server.use(express.json());
+
+
+// routes will go here
 
 // add your server code
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`Server up and running on ${port}`);
+  console.log(` > Server swerving on ${port}  `);
 });
