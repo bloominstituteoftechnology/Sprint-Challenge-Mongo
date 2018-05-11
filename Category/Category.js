cdc198dc55575536625505cf5a55ae6179927bda
2940cristian <2940cristian@gmail.com>
2018-05-11 08:36:37 -0700

    All schemas created, moving onto routers

---
+++ Category/Category.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const Category = new mongoose.Schema({
+    title: String
+})
---
+++ Expense/Expense.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+
+const Expense = new mongoose.Schema({
+    amount: Number,
+    description: String,
+    budget: {
+        type: ObjectId,
+        ref: "Budget"
+    }, // Monthly Spending
+    category: {
+        type: ObjectId,
+        ref: "Category"
+    } // Groceries
+})
+
+module.exports = mongoose.model('Expense', Expense);
