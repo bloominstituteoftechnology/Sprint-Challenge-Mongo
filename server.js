ab414a16039b18e53780bf99be60852ee1d57e8e
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-11 10:23:35 -0600

    server code

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express'); 
 const mongoose = require('mongoose');
 
-const server = express();
+const Budget = require('./routes/budgetRouter.js');
+const Category = require('./routes/categoryRouter.js');
+const Expense = require('./routes/expenseRouter.js');
 
-// add your server code
+const server = express();
+server.use(express.json());
+
+server.use('/budget', Budget);
+server.use('/category', Category);
+server.use('/expense', Expense);
+
+mongoose
+.connect('mongodb://localhost/mongoSprintChallenge')
+.then(connect => console.log('Successful connection to MongoDB.'))
+.catch(err => console.exception('Connection to database failed.'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
