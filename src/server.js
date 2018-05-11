ea60c46b3d5443fabc21018b0a2471f502ece03b
Jared Cuffe <jcuffe@gmail.com>
2018-05-11 07:44:43 -0700

    POST /budgets implemented with validation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
 const express = require('express'); // remember to install your npm packages
 const server = express();
+const { Budget, Expense, Category } = require('./models.js')
+const errors = require('./errors.js')
 
-// add your server code
+server.use(express.json())
 
+server.post('/budgets', (req, res, next) => {
+  const { title, budgetAmount } = req.body
+  if (!title) { next(errors.budgetTitle) }
+  if (!budgetAmount) { next(errors.budgetAmount)}
+  Budget.create({ title, budgetAmount })
+    .then(budget => res.send(budget))
+    .catch(() => next(errors.server))
+})
+
+server.use((err, req, res, next) => {
+  res.status(err.status).send({ error: err.message })
+})
 
 module.exports = server
