6140da5b9a88f873f68ad787039439feccf1ff90
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:46:12 -0700

    server.js - set up boilerplate

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
 
-const server = express();
 
-// add your server code
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
