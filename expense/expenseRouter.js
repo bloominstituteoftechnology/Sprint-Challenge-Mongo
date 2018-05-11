82ff8000ae37d0b60c9e7df0430172c972ac067a
cgoez <goez2001@gmail.com>
2018-05-11 09:58:00 -0700

    expenseRouter GET /

--- expense/expenseRouter.js
+++ expense/expenseRouter.js
@@ -6,12 +6,26 @@ const router = express.Router();
 
 // /api/expenses
 
+// GET /
+router.route("/").get((req, res) => {
+  Expense.find({})
+    .populate("budget", "category")
+    .then(expenses => {
+      res.status(200).json(expenses);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error getting expenses."
+      });
+    });
+});
+
 // POST /
 router.route("/").post((req, res) => {
   const { amount, description, budget, category } = req.body;
   const expense = new Expense(req.body);
 
-  // Make sure title and budgetAmout provided
+  // Make sure amount, description, budget and category provided
   if (amount && description && budget && category) {
     expense
       .save()
