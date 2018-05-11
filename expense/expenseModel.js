b8cd0c3b69271b6617fcbc4a645ddcfdc3aac833
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:13:03 -0700

    expenseModel.js - set up expense model

---
+++ expense/expenseModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Expense = mongoose.Schema({
+  
+});
+
+module.exports = mongoose.model('Expense', Expense);
