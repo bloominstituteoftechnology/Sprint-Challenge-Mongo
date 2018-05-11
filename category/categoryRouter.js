38f4c35705b8fb6668dc33902dea728350c8fb4b
sadrak105 <sadrak10@gmail.com>
2018-05-11 12:49:24 -0400

    title field required for category

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -3,15 +3,21 @@ const router = require('express').Router();
 const Category = require('./category.js');
 
 router.post('/', (req, res) => {
-    const category = new Category(req.body);
+    if (req.body.title) {
 
-    category.save()
-    .then(category => {
-        res.status(201).json(category)
-    })
-    .catch(err => {
-        res.status(500).json({ error: 'Could Not Post Category!' })
-    })
+        const category = new Category(req.body);
+    
+        category.save()
+        .then(category => {
+            res.status(201).json(category)
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could Not Post Category!' })
+        })
+    }
+    else {
+        res.status(400).json({ errorMessage: 'Title Field Required' })
+    }
 })
 
 router.get('/', (req, res) => {
