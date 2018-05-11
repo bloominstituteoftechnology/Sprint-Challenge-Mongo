11c242eea9b6142b45324690a40bb4796b303642
Justin Hong <jhong2009@gmail.com>
2018-05-11 09:05:27 -0700

    in server.js and db.js: set up db and server with routes. Set up project file structure

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const express = require('express'); // remember to install your npm packages
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const db = require("./data/db.js");
+const budgetRouter = require("./budget/budgetRouter");
+const expenseRouter = require("./expense/expenseRouter");
+const categoryRouter = require("./category/categoryRouter");
 
 const server = express();
 
-// add your server code
+db
+	.connectTo("mongoSprint")
+	.then(() => {
+		console.log("\n... API Connected to Database ...\n");
+	})
+	.catch(err => {
+		console.log("\n*** ERROR Connecting to Database ***\n", err);
+	});
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// Routes
+
+server.get("/", (req, res) => {
+	res.send("API Running...");
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`Server up and running on ${port}`);
+	console.log(`Server up and running on ${port}`);
 });
