2523738e615a9db9f12d09b788aa8d8ef9998111
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:26:12 -0700

    budgetRouter.js - set up routes for budget router

--- budget/budgetsRouter.js
+++ budget/budgetsRouter.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const Budget = require('./budgetModel');
 
-
+router.route('/')
+  .get((req, res) => {
+    Budget.find()
+      .then(budgets => res.json(budgets))
+      .catch(err => res.status(500).json(err));
+  })
+  .post((req, res) => {
+    const budget = new Budget(req.body);
+    budget.save()
+      .then(budget => res.status(201).json(budget))
+      .catch(err => res.status(500).json(err));
+  })
 
 module.exports = router;
