0f738fcd80fa8bf3e5c7c1390b44eb6a7077720e
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-11 11:06:13 -0400

    created directories and files for db collections

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
 const server = express();
 
+// const router = require('router_path')
+
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => res.send("Server is running."))
+// server.use('/path', routers)
+
+server.get('/', (req, res) => res.send("Server is running."));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
