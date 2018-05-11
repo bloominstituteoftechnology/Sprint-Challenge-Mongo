222ef7ce98c186ecaade8a3fbda36c569ed5a6e8
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-11 08:48:36 -0700

    Server set up
    set up server to my routers, and used express.json

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express'); // remember to install your npm packages
-
+const budgetRouter = require('./Budget/budgetRouter')
+const CategoryRouter = require('./Category/categoryRouter')
+const expenseRouter = require('./Expense/expenseRouter')
 const server = express();
 
 // add your server code
+server.use(express.json())
+
+server.use('/budgets',budgetRouter)
+server.use('/categories',CategoryRouter)
+server.use('/expenses',expenseRouter)
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
