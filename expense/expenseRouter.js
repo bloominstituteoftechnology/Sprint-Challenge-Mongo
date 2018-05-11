f5470ce1a02de6128420ca5d890ad28df5297da8
Chris <cjmarsh725@gmail.com>
2018-05-11 08:13:32 -0700

    Created expense routes

--- expense/expenseRouter.js
+++ expense/expenseRouter.js
@@ -4,6 +4,27 @@ const Expense = require('./Expense.js');
 
 const router = express.Router();
 
-// Endpoints
+router.post('/', (req, res) => {
+  const { amount, description, budget, category } = req.body;
+  if (amount && description && budget && category) {
+    const posted = new Expense({
+      amount: amount,
+      description: description,
+      budget: budget,
+      category: category
+    })
+    posted.save()
+      .then(result => res.status(201).json(result))
+      .catch(err => res.status(500).json(err));
+  } else {
+    res.status(400).json({msg: 'amount, description, budget, and category fields required'});
+  }
+})
+
+router.get('/', (req, res) => {
+  Expense.find().populate('budget').populate('category')
+    .then(expenses => res.status(200).json(expenses))
+    .catch(err => res.status(500).json(err));
+})
 
 module.exports = router;
