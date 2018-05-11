37e6141388ed9cc0500d3a82a46a8270ddcceb7e
sadrak105 <sadrak10@gmail.com>
2018-05-11 10:28:21 -0400

    added file folders

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const server = express();
 
 // add your server code
+mongoose
+.connect('mongodb://localhost/budgetdb')
+.then(mongo => {
+  console.log('connected to database');
+})
+.catch(err => {
+  console.log('Error connecting to database', err);
+});
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', function(req, res) {
+  res.status(200).json({ api: 'running' });
+});
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
