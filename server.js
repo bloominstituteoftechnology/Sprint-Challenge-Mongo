9a5efec634679c98361ee577636239c3bdaa803c
totsuya <supertatortots@gmail.com>
2018-05-11 12:48:35 -0400

    added basic server.js stuff

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express'); // remember to install your npm packages
-
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 const server = express();
 
 // add your server code
+
+mongoose
+  .connect('mongodb://localhost/budgetdb')
+  .then(mongo => {
+    console.log('\n.. API connected to Database! ...\n')
+  })
+  .catch(error => {
+    console.log('\n.. Could not connect! ..\n')
+  });
+
+
+  server.use(helmet());
+  server.use(express.json());
+
+  //api server.use here
+  server.use('api/budget')
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
