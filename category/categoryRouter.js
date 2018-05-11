3fcbb1c8a188bf44d145471f6e55a96d25f81d78
Chris <cjmarsh725@gmail.com>
2018-05-11 07:48:40 -0700

    Category routes

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -4,6 +4,22 @@ const Category = require('./Category.js');
 
 const router = express.Router();
 
-// Endpoints
+router.post('/', (req, res) => {
+  const { title } = req.body;
+  if (title) {
+    const posted = new Category({title: title})
+    posted.save()
+      .then(result => res.status(201).json(result))
+      .catch(err => res.status(500).json(err));
+  } else {
+    res.status(400).json({msg: 'title field required'});
+  }
+});
+
+router.get('/', (req, res) => {
+  Category.find().select('title -_id')
+    .then(categories => res.status(200).json(categories))
+    .catch(err => res.status(500).json(err));
+})
 
 module.exports = router;
