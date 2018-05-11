a4568e72b801b552d536a92f1d3d199bfa77572c
Harrison Brock <harrison@harrisonbrock.com>
2018-05-11 12:41:28 -0400

    ExpenseController.js -- Added Get to create a new Expense

--- controllers/ExpenseController.js
+++ controllers/ExpenseController.js
@@ -5,6 +5,16 @@ const expenseController = express.Router();
 
 expenseController
     .route('/')
+    .get((req, res) => {
+        Expense.find({})
+            .populate('budget category')
+            .then(expenses => {
+                res.status(200).json(expenses)
+            })
+            .catch(error => {
+                res.status(500).json(error)
+            })
+    })
     .post((req, res) => {
         const expense = new Expense(req.body);
 
