79a24601c015acc56202b9ce74583b70b0463a84
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:46:57 -0700

    server.js - imported routers abd database

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const db = require("./data/db.js");
+const budgetsRouter = require("./budget/budgetsRouter.js");
+const categoriesRouter = require("./category/categoriesRouter.js");
+const expensesRouter = require("./expense/expensesRouter.js");
 
 
 const port = process.env.PORT || 5000;
