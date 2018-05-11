6963300036e9105dc25fb84325e061d9f65dc39d
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-11 09:22:57 -0700

    added a database and connected to it

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
 
+const db = require('./data/data');
 const budgetRouter = require('./budget/budgetRouter.js');
 //const expenseRouter = require('./expense/expenseRouter.js');
 //const categoryRouter = require('./category/categoryRouter.js');
 
 const server = express();
+
+db 
+  .connectTo('budgetTracker')
+  .then(() => console.log('\n... API Connected to Database ...\n'))
+  .catch(err => console.log('\n*** ERROR Connecting to the Database ***\n'));
 
 // add your server code
 server.use(helmet());
@@ -14,6 +20,10 @@ server.use(express.json());
 server.use('./api/budgets', budgetRouter);
 //server.use('./api/expenses', expenseRouter);
 //server.use('./api/categories', categoryRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({api: 'running'});
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
