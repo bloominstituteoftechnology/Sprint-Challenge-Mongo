dfa1d114d911603a680172fc5f9d43aad148ab15
Jared Cuffe <jcuffe@gmail.com>
2018-05-11 07:28:28 -0700

    Mongoose connection and server initialization

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 const express = require('express'); // remember to install your npm packages
-
 const server = express();
 
 // add your server code
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`Server up and running on ${port}`);
-});
+
+module.exports = server
