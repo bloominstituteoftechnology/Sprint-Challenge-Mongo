3c4a9a82c45396fbdb68628ee73b1c0c3d68d74f
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-11 12:14:11 -0500

    set-up Expense model files

---
+++ expense/expenseRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Expense = require('./Expense.js');
+
+// Express Router
+const router = express.Router();
+
+// Set Endpoints Here
+
+
+// Module Export
+module.exports = router;
