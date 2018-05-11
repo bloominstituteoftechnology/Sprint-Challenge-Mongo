50cd4d8455e037411126cf525b81b673e6ecb200
Cruise Brantley <cruise.brantley@gmail.com>
2018-05-11 12:58:09 -0500

    Setting things up commit

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-const express = require('express'); // remember to install your npm packages
+const express = require("express"); // remember to install your npm packages
+const mongoose = require("mongoose");
+
+const budgetRouter = require("./budget/budgetRouter.js");
+const categoryRouter = require("./category/categoryRouter.js");
+const expenseRouter = require("./expense/expenseRouter.js");
 
 const server = express();
 
-// add your server code
+const db = mongoose.connect(`mongodb://localhost/sprint`);
+
+db
+  .connectTo("starwars")
+  .then(() => console.log("\n... API Connected to Database ...\n"))
+  .catch(err => console.log("\n*** ERROR Connecting to Database ***\n", err));
+
+server.use(express.json());
+
+server.use("/api/budget", budgetRouter);
+server.use("/api/expense", expenseRouter);
+server.use("/api/category", categoryRouter);
+
+server.get("/", (req, res) => res.send("API Running..."));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
