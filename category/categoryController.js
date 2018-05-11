36f10088a3696a3f61d983db43c4d29bff31712f
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-11 11:59:17 -0400

    post categories and get categories (title only)

--- category/categoryController.js
+++ category/categoryController.js
@@ -4,7 +4,22 @@ const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
-    res.send("Category router is functional.")
+    Category
+      .find()
+      .select('-_id title')
+      .then(categories => res.status(200).json(categories))
+      .catch(err => res.status(500).json({ error: "Cannot fetch any categories at this time." }))
+  })
+
+  .post((req, res) => {
+    const { title } = req.body;
+    if (!title || title === "") { res.status(400).send("A title is required to create a category.") }
+
+    const category = new Category({ title });
+    category
+      .save()
+      .then(savedCategory => res.status(201).json(savedCategory))
+      .catch(err => res.status(500).json({ error: "There was an issue saving your category." }))
   })
 
 module.exports = router;
