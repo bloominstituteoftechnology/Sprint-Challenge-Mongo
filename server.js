8d0fb387a9bcb17e042110e85692b23cb013fad9
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-11 08:41:10 -0700

    installed dependencies/ set up server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+
+const budgetRouter = require('./budget/budgetRouter.js');
+const expenseRouter = require('./expense/expenseRouter.js');
+const categoryRouter = require('./category/categoryRouter.js');
 
 const server = express();
 
 // add your server code
+server.use(helmet());
+server.use(express.json());
+
+server.use('./api/budget', budgetRouter);
+server.use('./api/expenses', expenseRouter);
+server.use('./api/categories', categoryRouter);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
