717b6f601418b982c32f920ccdde9dc7fdf89b25
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-11 12:20:58 -0400

    Robert Hogan. Project folders created, beginning to set up connection code.

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+// create a new db file
+const db = require('./db');
+//routers below
+const budgetRouter = require('./budgets/budgetRouter'); // budget router 
+const expenseRouter = require('./expenses/expenseRouter'); // expense router
+const categoryRouter = require('./categories/categoryRouter'); // category router 
 
 const server = express();
 
 // add your server code
+
+db
+.connectTo()
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
