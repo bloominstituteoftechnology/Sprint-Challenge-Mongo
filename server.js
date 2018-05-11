7c338b5897fbdf6043c40ded477cf15233820bc0
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-11 11:24:08 -0400

    expense router connected to server

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const helmet = require('helmet');
 const server = express();
 
 const budgetRouter = require('./budget/budgetController');
+const expenseRouter = require('./expenses/expenseController');
 
 server.use(helmet());
 server.use(express.json());
 
 server.use('/budget', budgetRouter);
+server.use('/expenses', expenseRouter);
 
 server.get('/', (req, res) => res.send("Server is running."));
 
