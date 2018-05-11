c828f45cca27fc406b1e8e01c84ce44d2b3cf809
Nedu Robert <nedumrobert@gmail.com>
2018-05-11 17:30:00 +0100

    connected database and routing

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/budgetTrackerdb')
+.then(mongo => {
+  console.log(`Sucessfully connected to database`)
+})
+.catch(err => {
+  console.log(`Error connecting to database: ${err}`)
+})
+
+const budgetRoutes = require('./routers/budgetRoutes');
+const expenseRoutes = require('./routers/expenseRoutes');
+const categoryRoutes = require('./routers/categoryRoutes');
 
 const server = express();
 
 // add your server code
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/budgets', budgetRoutes);
+server.use('/api/expenses', expenseRoutes);
+server.use('/api/category', categoryRoutes);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
