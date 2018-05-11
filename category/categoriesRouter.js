d74a171b8445513fcc87d081bafdd8f40532f9e0
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:27:10 -0700

    categoriesRouter.js - set up routes for category router

--- category/categoriesRouter.js
+++ category/categoriesRouter.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const Category = require('./categoryModel');
 
-
+router.route('/')
+  .get((req, res) => {
+    Category.find()
+      .then(categories => res.json(categories))
+      .catch(err => res.status(500).json(err));
+  })
+  .post((req, res) => {
+    const category = new Category(req.body);
+    category.save()
+      .then(category => res.status(201).json(category))
+      .catch(err => res.status(500).json(err));
+  })
 
 module.exports = router;
