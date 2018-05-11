7b018e4ec51a18803eb9fcb9379e35eac62237e6
Eric Andrade <eric.andrade@gmail.com>
2018-05-11 14:27:29 -0400

    finished

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/budgetdb')
+  .then(() => console.log('connected to mongo'))
+  .catch(err => console.log('could not connect to mongo'));
+
+// const budgetController = require('');
+// const categoryController = require('');
+// const expenseController = require('');
+
+// remember to install your npm packages
 
 const server = express();
 
 // add your server code
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running' });
+});
+
+// server.use('/api/budgets', budgetController);
+// server.use('/api/categories', categoryController);
+// server.use('/api/expenses', expenseController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
