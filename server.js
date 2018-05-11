74f2b9f1322e11bc33ca97447a6a11b3e6b479fb
Paul Fang <paulfang@pauls-mbp.home>
2018-05-11 11:00:47 -0700

    working on server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express'); // remember to install your npm packages
+const mongoose = require('mongoose');
+
+//new db file
+const db = require('./db');
+//routers
+const budgetRouter = require('./budgets/budgetRouter'); // budget router 
+const expenseRouter = require('./expenses/expenseRouter'); // expense router
+const categoryRouter = require('./categories/categoryRouter'); // category router 
 
 const server = express();
 
 // add your server code
+
+db
+.connectTo()
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
