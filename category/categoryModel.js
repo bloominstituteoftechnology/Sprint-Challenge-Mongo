d0b2e11886728e2346865770e4cc2429a67b4faf
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:11:34 -0700

    categoryModel.js - populated schema

--- category/categoryModel.js
+++ category/categoryModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Category = mongoose.Schema({
-  
+  title: String,
 });
 
 module.exports = mongoose.model('Category', Category);
