8d74036d534ad6112675ce6c153949b36855162a
cgoez <goez2001@gmail.com>
2018-05-11 08:43:01 -0700

    server.js connect db

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express'); // remember to install your npm packages
+const express = require("express"); // remember to install your npm packages
 
 const server = express();
 
 // add your server code
+db
+  .connectTo("budgetTracker")
+  .then(() => console.log("\n=== API Connected to Budget DB ===\n"))
+  .catch(error => console.log("\n*** Error Connecting to Budget DB!!! ***\n"));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
