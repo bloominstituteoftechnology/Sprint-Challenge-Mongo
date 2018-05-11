0e25a4475c0fae768e4ee2d498ba01cf8a4b9d6b
Yasin Shuman <email@yasin.io>
2018-05-11 10:57:36 -0700

    connected db

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express'); // remember to install your npm packages
+const express = require("express"); // remember to install your npm packages
+const helmet = require("helmet");
+const db = require("./data/db.js");
 
 const server = express();
 
 // add your server code
+db
+  .connectTo("budget")
+  .then(() => console.log("\n... API Connected to Database ...\n"))
+  .catch(err => console.log("\n*** ERROR Connecting to Database ***\n", err));
+
+server.use(helmet());
+server.use(express.json());
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
