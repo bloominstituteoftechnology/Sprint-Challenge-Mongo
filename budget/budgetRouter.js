476f46b1ee8f067d3d8206bf6957a08bb730f807
sadrak105 <sadrak10@gmail.com>
2018-05-11 10:38:07 -0400

    setup files to be used with dependencies

---
+++ budget/budgetRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const Budget = require('./budget.js');
+
+const router = express.Router();
+
+
+module.exports = router;
---
+++ expense/expenseRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const Expense = require('./expense.js');
+
+const router = express.Router();
+
+
+module.exports = router;
