52376e30337d45cbe32e2be13efb010afc4d2a0c
Devon Bacon <devon.bacon@gmail.com>
2018-05-11 09:47:36 -0700

    server.js - set up connection to mongo

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const budgetsRouter = require("./budget/budgetsRouter.js");
 const categoriesRouter = require("./category/categoriesRouter.js");
 const expensesRouter = require("./expense/expensesRouter.js");
 
+const server = express();
+
+db
+	.connectTo("budgetTracker")
+	.then(() => console.log("\n... API Connected to Database ...\n"))
+	.catch(err => console.log("\n*** ERROR Connecting to Database ***\n", err));
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
