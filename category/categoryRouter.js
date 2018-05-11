7c10a788086cc570db3b8003fb5b6a244f0f8eb9
cgoez <goez2001@gmail.com>
2018-05-11 10:58:44 -0700

    categoryRouter DELETE /:id

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -22,18 +22,18 @@ router.route("/").get((req, res) => {
 
 // GET /:id
 router.route("/:id").get((req, res) => {
-    const { id } = req.params;
-  
-    Category.findById(id)
-      .then(category => {
-        res.status(200).json(category);
-      })
-      .catch(error => {
-        res.status(500).json({
-          error: "There was an error getting the specified category."
-        });
+  const { id } = req.params;
+
+  Category.findById(id)
+    .then(category => {
+      res.status(200).json(category);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error getting the specified category."
       });
-  });
+    });
+});
 
 // POST /
 router.route("/").post((req, res) => {
@@ -61,24 +61,45 @@ router.route("/").post((req, res) => {
 
 // PUT /:id
 router.route("/:id").put((req, res) => {
-    const { id } = req.params;
-    const update = req.body;
-  
-    Category.findByIdAndUpdate(id, update)
-      .then(category => {
-        if (category) {
-          res.status(200).json(category);
-        } else {
-          res.status(404).json({
-            error: "Category not found!"
-          });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({
-          error: "There was an error updating the category."
+  const { id } = req.params;
+  const update = req.body;
+
+  Category.findByIdAndUpdate(id, update)
+    .then(category => {
+      if (category) {
+        res.status(200).json(category);
+      } else {
+        res.status(404).json({
+          error: "Category not found!"
         });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error updating the category."
       });
-  });
+    });
+});
+
+// DELETE /:id
+router.route("/:id").delete((req, res) => {
+  const { id } = req.params;
+
+  Category.findByIdAndRemove(id)
+    .then(category => {
+      if (category) {
+        res.status(200).json(category);
+      } else {
+        res.status(404).json({
+          error: "Category not found!"
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error deleting the category."
+      });
+    });
+});
 
 module.exports = router;
