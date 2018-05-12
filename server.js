54c8a57548804e82611c54a5dc18e45c7205a87b
axhon <axhonruiz@gmail.com>
2018-05-11 20:17:46 -1000

    finish routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import helmet from 'helmet';
 import mongoose from 'mongoose';
 
 import { BudgetModel } from './models/budget';
+import { CategoryModel } from './models/category';
+import { ExpenseModel } from './models/expense';
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -19,14 +21,68 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.post('/budgets', async (req, res) => {
+const asyncHandler = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(e => next(e));
+
+const asyncRouteGenerator = fn => asyncHandler(fn);
+
+const budgetsPostRoute = async (req, res, next) => {
   const { body } = req;
   // validate body
+  const newBudget = await new BudgetModel(body).save();
+  await res.json(newBudget);
+};
 
-  const newBudget = await new BudgetModel(body);
-  console.log(newBudget);
-  await res.send('hey');
-});
+server.post('/budgets', asyncRouteGenerator(budgetsPostRoute));
+
+const categoriesPostRoute = async (req, res, next) => {
+  const { body } = req;
+  // validate categories
+  const newCategory = await new CategoryModel(body).save();
+  await res.json(newCategory);
+};
+
+server.post('/categories', asyncRouteGenerator(categoriesPostRoute));
+
+const categoriesGetAllRoute = async (req, res, next) => {
+  const categories = await CategoryModel.find()
+    .select('title')
+    .exec();
+  await res.json(categories);
+};
+
+server.get('/categories', asyncRouteGenerator(categoriesGetAllRoute));
+
+const expensesPostRoute = async (req, res, next) => {
+  const {
+    body: { budget, category, description, amount }
+  } = req.body;
+
+  const expense = await new ExpenseModel({
+    budget,
+    category,
+    description,
+    amount
+  }).save();
+  res.json(
+    await expense
+      .populate('category')
+      .populate('budget')
+      .execPopulate()
+  );
+};
+
+server.post('/expenses', asyncRouteGenerator(expensesPostRoute));
+
+const expensesGetAllRoute = async (req, res, next) => {
+  const expenses = await ExpenseModel.find()
+    .populate('category')
+    .populate('budget')
+    .exec();
+  res.json(expenses);
+};
+
+server.get('/expenses', asyncRouteGenerator(expensesGetAllRoute));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
