c04a82aaf5d3e7218ff47556811e0e638ebe535f
axhon <axhonruiz@gmail.com>
2018-05-11 18:10:03 -1000

    added error middleware for non-promises

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 import express from 'express'; // remember to install your npm packages
+import helmet from 'helmet';
 
 const server = express();
 
 // add your server code
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+
+// async error catcher
+const asyncMiddleware = fn => (error, req, res, next) => {
+  const isPromise = fn instanceof Promise;
+  if (!isPromise) return next(error);
+};
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
