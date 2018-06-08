9f0c2f7db8b2e90b63f4114a4efb5a49f9e0f654
jesuarva <jean.ariza@jesuarva.com>
2018-06-08 19:32:15 +0200

    BUDGET: define specific routes

--- budget/budget.router.js
+++ budget/budget.router.js
@@ -6,6 +6,20 @@ const myFactory = require('../myTools/routerFactory');
 
 const router = express.Router();
 
-myFactory.routerFactory(router, Budget)('to_populate_1 to_populate_2');
+router.route('/').get(noAllowed);
+
+router
+  .route('/:id')
+  .get(noAllowed) // TODO: make this Router aggregate the info.
+  .put(noAllowed);
+
+myFactory.routerFactory(router, Budget)('expenses');
 
 module.exports = router;
+
+/**
+ *  :CUSTOM MIDDLEWARES
+ */
+function noAllowed(req, res, next) {
+  res.status(404).json({ message: 'Action no allowed' });
+}
