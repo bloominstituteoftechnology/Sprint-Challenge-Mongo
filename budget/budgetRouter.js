d4f6c9d6ce958ee18c18607ba9db6484960d2a42
Thuy <tpham045@hotmail.com>
2018-06-08 16:37:54 +0000

    feat: POSt request /category

--- budget/budgetRouter.js
+++ budget/budgetRouter.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
-        console.log(Budget)
         Budget
             .find({})
             .then(response => res.status(200).json({ data: response }))
@@ -14,9 +13,14 @@ router
     })
     .post((req, res) => {
         const newBudget = req.body
-        Budget.create(newBudget)
-            .then(response => res.status(200).json({ data: response }))
-            .catch(err => res.status(500).json(err))
+        if (newBudget.title && newBudget.budgetAmount) {
+            Budget.create(newBudget)
+                .then(response => res.status(200).json({ data: response }))
+                .catch(err => res.status(500).json(err))
+        }
+        else {
+            res.status(400).json({ message: 'Please provide budget title and amount.' })
+        }
     })
 
 module.exports = router;
--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -4,9 +4,24 @@ const Category = require('./Category.js');
 const router = express.Router();
 
 router
+router
     .route('/')
     .get((req, res) => {
-
+        Category
+            .find({})
+            .then(response => res.status(200).json({ data: response }))
+            .catch(err => res.status(500).json(err))
+    })
+    .post((req, res) => {
+        const newCategory = req.body
+        if (newCategory.title) {
+            Category.create(newCategory)
+                .then(response => res.status(200).json({ data: response }))
+                .catch(err => res.status(500).json(err))
+        }
+        else {
+            res.status(400).json({ message: 'Please provide category title.' })
+        }
     })
 
 module.exports = router;
