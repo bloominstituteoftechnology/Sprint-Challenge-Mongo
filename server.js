e7af3c2c66da9d9719d9c6986cc2cb29ac6e65f4
udykas <udykas@gmail.com>
2018-06-08 11:49:18 -0400

    packages installed, server set up, server runnning and connected to db

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const server = express();
 
 // add your server code
+
+server.use(helmet());
+server.use(cors())
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running' })
+});
+
+//MONGOOSE
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect('mongodb://localhost/budgetdb')
+  .then(mongo => {
+    console.log('Mongoose connected us to our DB');
+  })
+  .catch(err => {
+    console.log('Database connection failed', err);
+  })
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
