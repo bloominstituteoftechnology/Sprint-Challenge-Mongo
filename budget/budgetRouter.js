65f08db54f51ca6cb5e1db4f2835934c2107f40d
udykas <udykas@gmail.com>
2018-06-08 14:01:03 -0400

    Can delete budgets by id now

--- budget/budgetRouter.js
+++ budget/budgetRouter.js
@@ -32,5 +32,23 @@ router
                 res.status(500).json({ error: "There was an error saving the budget to the database" })
             })
     })
+    
+
+router
+    .route('/:id')
+    .delete((req, res) => {
+        Budget
+            .findByIdAndRemove(req.params.id)
+            .then(budget => {
+                if(budget){
+                    res.status(200).json({ success: `Budget with the id ${req.params.id} has been removed from the database` })
+                } else {
+                    res.status(404).json({ error: "The budget with the specified ID does not exist" })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The budget could not be removed" })
+            })
+    })
 
 module.exports = router;
