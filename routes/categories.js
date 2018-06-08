8db5eb5b70428a50df43e87fa5f48d04a50072c8
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-08 13:14:35 -0400

    Created GET endpoint to retrieve the list of categories - Returns titles only

--- routes/categories.js
+++ routes/categories.js
@@ -2,17 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Category = require('../models/category');
 
-router.post('/', (req, res) => {
-  const { title } = req.body;
-  const newCategory = new Category({ title });
+router
+  .post('/', (req, res) => {
+    const { title } = req.body;
+    const newCategory = new Category({ title });
 
-  newCategory.save()
-    .then(category => {
-      res.status(201).json(category);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-})
+    newCategory.save()
+      .then(category => {
+        res.status(201).json(category);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  })
+  .get('/', (req, res) => {
+    Category.find()
+      .select({
+        title: 1,
+        _id: 0
+      })
+      .then(categories => {
+        res.status(200).json(categories);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  })
 
 module.exports = router;
