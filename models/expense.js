534b2395455da1f9046828c82104e7ce0d31e9a3
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-08 15:50:22 -0400

    Added additional validation for ObjectId types

--- models/expense.js
+++ models/expense.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const objectIdValid = (model, value, cb) => {
+  mongoose.model(model)
+    .count({ _id: value }, (error, count) => {
+      cb(error || count === 0 ? false : true);
+    })
+}
+
 const expenseSchema = new mongoose.Schema({
   amount: {
     type: Number,
@@ -16,12 +23,22 @@ const expenseSchema = new mongoose.Schema({
   budget: {
     type: ObjectId,
     required: [true, 'Please include an id for the budget that this expense belongs to'],
-    ref: 'Budget'
+    ref: 'Budget',
+    validate: {
+      isAsync: true,
+      validator: (value, cb) => objectIdValid('Budget', value, cb),
+      message: 'The budget you are trying to link this expense to does not exist'
+    }
   },
   category: {
     type: ObjectId,
     required: [true, 'Please include an id for the category that this expense belongs to'],
-    ref: 'Category'
+    ref: 'Category',
+    validate: {
+      isAsync: true,
+      validator: (value, cb) => objectIdValid('Category', value, cb),
+      message: 'The category you are trying to link this expense to does not exist'
+    }
   }
 });
 
