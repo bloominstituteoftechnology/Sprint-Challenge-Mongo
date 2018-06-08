d4b1c09378d091c296dada49c14c48faf84f4d6d
Jacob Henry <jacobiswinning@gmail.com>
2018-06-08 10:12:12 -0600

    setup schema for Budget, Expense and Category

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
 
 const server = express();
 
+
+// middlewarez
+server.use(helmet());
+server.use(express.json());
+
 // add your server code
+
+server.get('/', (req, res) => res.send('API Running...'));
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
