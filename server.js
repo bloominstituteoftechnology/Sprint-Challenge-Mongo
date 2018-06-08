4b1df069c7319ee2df4f11f8339a82346b7502bb
Jacob Henry <jacobiswinning@gmail.com>
2018-06-08 11:13:50 -0600

    added controllers, finished server.js

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const budgetRouter = require('./routes/budgetRouter.js');
 const expenseRouter = require('./routes/expenseRouter.js');
 const categoryRouter = require('./routes/categoryRouter.js');
 
+
+db
+  .connectTo('budgetTracker')
+  .then(() => console.log('\n... API Connected to Database ...\n'))
+  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+
 // middlewarez
 server.use(helmet());
 server.use(express.json());
 
 // add your server code
-
+server.use('/api/budget', budgetController);
+server.use('/api/expense', expenseController);
+server.use('/api/category', categoryController);
 
 
 server.get('/', (req, res) => res.send('API Running...'));
