81bb4286f251125f16d051e666f9cea008b3bf4c
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-08 09:34:15 -0700

    create POST for budget

--- budgets/budgetRouter.js
+++ budgets/budgetRouter.js
@@ -2,5 +2,26 @@ const express = require('express')
 const Budget = require('./Budget.js')
 const router = express.Router()
 
+router
+    .route('/')
+    .post((req, res) => {
+        const { title, budgetAmount } = req.body
+        const newBudget = new Budget({ title, budgetAmount })
+        if (!title || !budgetAmount) {
+            res.status(400).json({ 
+                userError: "Please provide a title and amount for your budget" 
+            })
+        } else {
+            newBudget.save()
+                .then( budget => {
+                    res.status(201).json(budget)
+                })
+                .catch( err => {
+                    res.status(500).json({
+                        error: err.message
+                    })
+                })
+        }
+    })
 
 module.exports = router
