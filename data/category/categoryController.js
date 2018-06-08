4fafe9e6f19fe590b8ccf885cfd8c329091e44ab
A99 <A99@99s-macbook-pro.home>
2018-06-08 19:48:53 +0200

    Added Expense queries & modified category GET

--- data/category/categoryController.js
+++ data/category/categoryController.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const Category = require('./category.js');
 
+
 router.route('/')
     .get((req, res) => {
-        Category.find()
+        Category.find({}, '-_id -__v')
             .then(response => res.json(response))
             .catch(err => res.status(500).json({ error: err.message }));
     })
     .post((req, res) => {
         const newCategory = new Category({ title } = req.body);
         newCategory.save()
-            .then(response => res.json(response))
+            .then(response => res.status(201).json(response))
             .catch(err => res.status(500).json({ error: err.message }));
     })
 
--- data/expense/expenseController.js
+++ data/expense/expenseController.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const Expense = require('./expense.js');
 
-router.route('/').post((req, res) => {
-    
-})
+const populateQuery = [{
+    path: 'category'
+}, {
+    path: 'budget'
+}]
+
+
+router.route('/')
+    .get((req, res) => {
+        Expense.find()
+            .populate(populateQuery)
+            .then(response => res.json(response))
+            .catch(err => json.status(500).json({ error: err.message }));
+    })
+    .post((req, res) => {
+        const newExpense = new Expense({ amount, description, budget, category } = req.body);
+        newExpense.save()
+            .then(response => res.status(201).json(response))
+            .catch(err => json.status(500).json({ error: err.message }));
+    })
 
 module.exports = router;
