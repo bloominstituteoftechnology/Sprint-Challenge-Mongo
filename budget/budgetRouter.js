5780c967cbe8212fb8d1a9c15db2fbb49867f269
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-06-08 11:10:30 -0700

    server/db setup and connection. writing budgetRouter

---
+++ budget/budgetRouter.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const Friend = require('./budget');
+
+router
+    .route('/')
+    .get((req, res) => {
+       Budget.find()
+        .then(response => {
+            res.status(200).json(budget);
+        })
+        .catch(err => res.status(500).json({error: 'Error fetching budget'}))
+    })
+
+    .post((req, res) => {
+        const { title, budgetAmount } = req.body;
+        const newBudget = new Budget({ title, budgetAmount });
+        console.log(newBudget);
+        newBudget
+            .save()
+            .then(savedBudget => {
+                res.status(201).json(savedBudget);
+            })
+            .catch(err => {
+                res.status(422).json({ error: 'Error saving budget'});
+            });
+    });
+
+    module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express'); // remember to install your npm packages
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const server = express();
-
+const db = require('./db.js');
 // add your server code
+const budgetRouter = require('./budget/budgetRouter.js');
+const expenseRouter = require('./expense/expenseRouter.js');
+const categoryRouter = require('./budget/categoryRouter.js');
+
+db
+  .connectTo('budgetTracker')
+  .then(() => console.log('\n... API Connected to Database ...\n'))
+  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+
+server.use('/api/budget', budgetRouter);
+server.use('/api/expense', expenseRouter);
+server.use('/api/category', categoryRouter);
+
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+  res.status(201).json('Server Running')
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
