3a12cdd5520a9edf6654229d3a3bcf6d5aee0714
bbenefield89 <bsquared18@gmail.com>
2018-06-08 15:43:24 -0400

    push

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express'); // remember to install your npm packages
+const mongoose = require('mongoose');
 
 const server = express();
+const budget = require('./routes/budgetRoute');
 
-// add your server code
+server.use(express.json());
+server.use('/api/budgets', budget);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
+mongoose.connect('mongodb://localhost/sandbox', () => console.log('Good'));
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
