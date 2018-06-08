a5080127096efaa7d950fe8e1f4a8edf114a9382
Zach Campbell <beazlebubbles@gmail.com>
2018-06-08 09:48:22 -0600

    Fixed refs

--- expenses/Expense.js
+++ expenses/Expense.js
@@ -5,8 +5,8 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Expense = new mongoose.Schema({
   description: { type: String, required: true },
   amount: { type: Number, required: true },
-  budget: {type: ObjectId, ref: "Character" },
-  category: {type: ObjectId, ref: "Specie" }
+  budget: {type: ObjectId, ref: "Budget" },
+  category: {type: ObjectId, ref: "Category" }
 });
 
 module.exports = mongoose.model('Expense', Expense);
