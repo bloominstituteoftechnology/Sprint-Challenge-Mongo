fcf6c6b0ce869640ab13f66dbabb4bf18231a30e
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-08 11:35:54 -0500

    File stucture set, Server up and running, Mongoose connected to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express'); // remember to install your npm packages
-
+const express = require('express');
+const mongoose = require('mongoose');
 const server = express();
 
 // add your server code
+const budgetController = require('./budgetTracker/budgetController');
+const categoryController = require('./budgetTracker/categoryController');
+const expenseController = require('./budgetTracker/expenseController');
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'runnig' }); 
+});
+
+server.use('/api/budgets', budgetController);
+server.use('/api/category', categoryController);
+server.use('/api/expense', expenseController);
 
 const port = process.env.PORT || 5000;
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbbudgetTracker', {}, err => {
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB');
+});
+
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
