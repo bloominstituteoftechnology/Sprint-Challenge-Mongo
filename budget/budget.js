77b3a2c08663f53cc63fafa0a803823989e2c8c4
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-08 11:23:19 -0500

    created budget model

--- budget/budget.js
+++ budget/budget.js
@@ -3,4 +3,23 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Schema = mongoose.Schema;
 
 
+const definition = {
+        title: {
+            type: String,
+            required: true
+        },
+        budgetAmount: {
+            type: Number,
+            required: true
+        }
+}
+
+const options = {
+    timestamps: true
+};
+
+const budgetSchema = new Schema(definition, options);
+
+const budgetModel = mongoose.model('Budget', budgetSchema); // name of collection will default to budgets
+
 module.exports = mongoose.model('Budget', Budget);
