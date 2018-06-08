1d985b8f3167dc64f25e3cb199e418ca4da6f573
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-08 12:00:26 -0500

    added server code

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+// install cors later
+
+const mongoose = require('mongoose');
+
+// connect to mongo
+mongoose
+  .connect('mongodb://localhost/expenseTrackerDB')
+  .then(mongo => {
+    console.log('\n... API Connected to Database ...\n')
+  })
+  .catch(err => {
+    console.log('\n*** ERROR Connecting to Database ***\n', err)
+  });
+
+  const budgetController = require('./budget/budgetController');
+  const categoryController = require('./category/categoryController');
+  const expenseController = require('./expense/expenseController');
 
 const server = express();
 
-// add your server code
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/budget', budgetController);
+server.use('/api/categories', categoryController);
+server.use('/api/expenses', expenseController);
+
+server.get('/', (req, res) => res.send('API Running...'));
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
