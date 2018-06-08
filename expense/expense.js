c64b3304fca60cb64b0c8e5fe2cb0fa6a4761004
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-08 11:31:01 -0500

    created expense model

--- expense/expense.js
+++ expense/expense.js
@@ -3,4 +3,31 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Schema = mongoose.Schema;
 
 
+const definition = {
+        amount: {
+            type: Number, 
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        budget: {
+            type: ObjectId,
+            ref: 'Budget'
+        },
+        category: {
+            type: ObjectId, 
+            ref: 'Category'
+        }
+}
+
+const options = {
+    timestamps: true
+}
+
+const expenseSchema = new Schema(definition, options);
+
+const expenseModel = mongoose.model('Expense', expenseSchema);
+
 module.exports = mongoose.model('Expense', Expense);
