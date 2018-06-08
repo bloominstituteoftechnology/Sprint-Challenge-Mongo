a53093f166cf0014e31232edc3a740aa8c3e09aa
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-08 12:40:50 -0500

    POST and GET end points established with /expenses but relationship with categories and budget not established

--- budgetTracker/expenseController.js
+++ budgetTracker/expenseController.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
+const Expense = require('./expense.js');
+
+router
+.route('/')
+.get((req, res) => {
+    Expense.find()
+    .then(expense => {
+        res.status(200).json(expense);
+    })
+    .catch(err => res.status(500).json({ error: 'The expense information could not be retrieved.' }));
+})
+.post((req, res) => {
+    const { amount, description, budget, category } = req.body;
+    const newExpense = new Expense({ amount, description, budget, category });
+newExpense
+    .save()
+    .then(savedExpense => {
+        res.json(savedExpense);
+    })
+})
 
 
 
