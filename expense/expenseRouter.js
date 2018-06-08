4ad74be63357ac6173e562957e155fc8f19018fa
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-06-08 16:42:42 -0700

    Fixed all type. ALL UP AND RUNNING

--- expense/expenseRouter.js
+++ expense/expenseRouter.js
@@ -4,22 +4,22 @@ const Expense = require('./expense');
 
 router
     .route('/')
-    .get((req, res) => {
-        Expense
-        .find()
-            .populate('budget')
-            .populate('category')
-            .then(expenses => res.json(expense))
-            .catch(err => res.status(500).json({error: err}));
-        })
-        
+        .get((req, res) => {
+            Expense
+                .find()
+                    .populate('budget')
+                    .populate('category')
+                    .then(foundExpense => res.json(foundExpense))
+                    .catch(err => res.status(500).json({error: err}));
+                })
+            
         .post((req, res) => {
             const { amount, description, budget, category } = req.body;
             const newExpense = new Expense({ amount, description, budget, category });
             newExpense
-            .save()
-            .then(expenses => res.join(expenses))
-            .catch(err => res.status(500).json({error: err}));
-    })
+                .save()
+                .then(expenses => res.json(expenses))
+                .catch(err => res.status(500).json({error: err}));
+        })
 
 module.exports = router;
