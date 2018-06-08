faa980d6b5caf6f4e7ce99212d47ffaff86ed27a
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-08 08:40:36 -0700

    get route added for budget controller

---
+++ expenses/expensesController.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const Expense = require('./expensesModel.js');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get((req, res) => {
+    Expense.find()
+      .then(expenses => {
+        res.status(200).json(expenses);
+      })
+      .catch(err => {
+        res.status(500).json([{ error: err.message }]);
+      });
+  })
---
+++ expenses/expensesModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Expense = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  budget: {
+    type: ObjectId,
+    required: true
+  },
+  category: {
+    type: ObjectId,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Expense", Expense);
