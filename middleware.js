44640906dc8a1c91ff28aa7696b88b2031af9cf7
Zach Campbell <beazlebubbles@gmail.com>
2018-06-08 11:19:47 -0600

    Added sanitize middleware

--- middleware.js
+++ middleware.js
@@ -9,6 +9,41 @@ const getMiddleware = (goose) => {
   }
 }
 
+//middleware to sanitize the body
+const sanitizeMiddleWare = (type) => {
+  return (req, res, next) => {
+    if (type === "budget") {
+      const budget = ({ title, budgetAmount } = req.body);
+      if (title === undefined || budgetAmount === undefined) {
+        res.status(400).json({ errorMessage: "Please provide a title and budget amount." })
+      }
+      req.saneBody = budget;
+    }
+
+    if (type === "category") {
+      const category = ({ title } = req.body);
+      if (title === undefined) {
+        res.status(400).json({ errorMessage: "Please provide a title." })
+      }
+      req.saneBody = category;
+    }
+
+    if (type === "expense") {
+      const expense = ({ amount, description, budget, category } = req.body);
+      if (amount === undefined || description === undefined || budget === undefined || category === undefined) {
+        res.status(400).json({ errorMessage: "Please provide an amount, description, budget, and category." })
+      }
+      req.saneBody = expense;
+    }
+
+    next();
+  }
+}
+
 module.exports = {
   getMiddleware: getMiddleware
 };
+
+
+
+const friend = ({ firstName, lastName, age, contactInfo } = req.body);
