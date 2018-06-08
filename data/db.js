66c78fd9048011da85962e4c307848d4425bea2e
Lavell Burton <taylormade800@gmail.com>
2018-06-08 11:47:35 -0700

    updating files needs work will return shortly

--- data/db.js
+++ data/db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-module.exports = {
-    connectTo: function (database = 'sandbox', host = 'localhost') {
-        return mongoose.connect(`mongod://${host}/${database}`)
-    },
-};
+// module.exports = {
+//     connectTo: function (database = 'sandbox', host = 'localhost') {
+//         return mongoose.connect(`mongod://${host}/${database}`)
+//     },
+// };
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 
 // const db = require('/data/db.js');
@@ -17,7 +17,7 @@ const server = express();
 //   .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
 
 server.use(helmet());
-server.use(cors());
+// server.use(cors());
 server.use(express.json());
 
 server.use('api/budget', budgetRouter); // budgetRouter middleware
@@ -31,7 +31,7 @@ server.get('/', (req, res) => res.send('API Running...'));
 
 const port = process.env.PORT || 5000;
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/dbbudget', {}, err => {
+mongoose.connect('mongodb://localhost/dbcategories', {}, err => {
   if (err) console.log(err);
   console.log('Mongoose connected to our DB');
 });
