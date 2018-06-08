f4db996db2a669a3f1d5ce31d83524f177f8588f
jesuarva <jean.ariza@jesuarva.com>
2018-06-08 18:41:14 +0200

    DEFINE Budget Schema

--- budget/Budget.model.js
+++ budget/Budget.model.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const Schema = mongoose.Schema;
 
-const Budget = new Schema({});
+const Budget = new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  budgetAmount: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function(amount) {
+        return amount >= 0;
+      },
+      message: 'Budget must be equal or greater than zero',
+    },
+  },
+});
 
 module.exports = mongoose.model('Budget', Budget);
