bf20fff6440d7a1ee6caa46701e8d62bbed18c70
Zach Campbell <beazlebubbles@gmail.com>
2018-06-08 09:30:10 -0600

    Boilerplated basic server from previous projects

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// const budgetController = require('./budgets/budgetController.js');
+// const expenseController = require('./expenses/expenseController.js');
+// const categoryController = require('./categories/categoryController.js');
+
 
 const server = express();
 
-// add your server code
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', function(req, res) {
+  res.json({ api: 'running' });
+});
+
+// server.use('/api/budgets', budgetController);
+// server.use('/api/expenses', expenseController);
+// server.use('/api/categories', categoryController);
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbFriends', {}, error => {
+  if (error) console.log("Database connection failed");
+  else console.log("Successfully Connected to MongoDB");
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`Server up and running on ${port}`);
+  console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
