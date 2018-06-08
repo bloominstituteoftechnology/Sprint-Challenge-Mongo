265558ca44164ec96227ec09fb048e176c43d0a2
Jacob Henry <jacobiswinning@gmail.com>
2018-06-08 10:39:24 -0600

    added data file and db.js, wired up the routes in server.js

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const helmet = require('helmet');
 
 const server = express();
 
+const db = require('./data/db.js');
+const budgetRouter = require('./routes/budgetRouter.js');
+const expenseRouter = require('./routes/expenseRouter.js');
+const categoryRouter = require('./routes/categoryRouter.js');
 
 // middlewarez
 server.use(helmet());
 server.use(express.json());
 
 // add your server code
+
+
 
 server.get('/', (req, res) => res.send('API Running...'));
 
