40cc25c99503fb2756a04d7923ce11d55ec30b1e
Steve <shalomlandsman@gmail.com>
2018-06-07 22:27:01 -0400

    post and get to expenses; mvp

--- expense/Expense.js
+++ expense/Expense.js
@@ -4,8 +4,8 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Expense = mongoose.Schema({
     amount: Number,
     description: String,
-    budget: [{ type: ObjectId, ref: 'Budget' }],
-    category: [{ type: ObjectId, ref: 'Category' }]
+    budget: { type: ObjectId, ref: 'Budget' },
+    category: { type: ObjectId, ref: 'Category' }
 });
 
 module.exports = mongoose.model('Expense', Expense);
--- expense/expenseRouter.js
+++ expense/expenseRouter.js
@@ -5,6 +5,24 @@ const Expense = require('./Expense.js');
 const router = express.Router();
 
 router
+    .route('/')
+    .get((req, res) => {
+        Expense.find()
+          .populate('budget')
+          .populate('category')
+          .then(expenses => {
+            res.status(200).json(expenses);
+          })
+          .catch(err => res.status(500).json({ errorMessage: "The expense information could not be retrieved." }));
+    })
+    .post((req, res) => {
+        const { amount, description, budget, category } = req.body;
+        const newExpense = new Expense({ amount, description, budget, category });
+        newExpense.save() 
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json({ error: err.message }));
+    });
+
 
 module.exports = router;
 
