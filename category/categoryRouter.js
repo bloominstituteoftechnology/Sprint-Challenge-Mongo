a211cee46229e3a9cdd95ac3ffdb7af404dfed25
udykas <udykas@gmail.com>
2018-06-08 13:04:31 -0400

    Can now GET and POST expenses

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -7,6 +7,7 @@ router
     .get((req, res) => {
         Category
             .find()
+            .select('title')
             .then(category => {
                 res.status(200).json(category)
             })
