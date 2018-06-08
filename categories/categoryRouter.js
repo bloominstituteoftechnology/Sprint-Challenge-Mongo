b566d8e6eb87c784a1c0d265c098dda396e076f3
Bradiowave <divebombwalker@gmail.com>
2018-06-08 09:31:40 -0700

    built out category post and get methods

--- categories/categoryRouter.js
+++ categories/categoryRouter.js
@@ -4,6 +4,18 @@ const Category = require('./Category.js');
 
 const router = express.Router();
 
-
+router
+    .route('/')
+        .post((req, res) => {
+            const newCategory = (new Category ( { title } = req.body ));
+            newCategory.save()
+                .then(category => res.status(201).json(category))
+                .catch(err => res.json(err))
+        })
+        .get((req, res) => {
+            Category.find( {}, { "title": 1 , "_id": 0 })
+                .then(categories => res.json(categories))
+                .catch(err => res.json(err))
+        })
 
 module.exports = router;
