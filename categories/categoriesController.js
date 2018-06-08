153f7d8304b4e07303653202af351c587199e338
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-08 09:15:20 -0700

    post route created for categories

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -15,3 +15,28 @@ router
         res.status(500).json([{ error: err.message }]);
       });
   })
+  .post((req, res) => {
+    const { title } = req.body;
+    const newCategory = new Category({ title });
+    if (!title) {
+      res.status(400).json([{ error: "Title required." }]);
+      return;
+    };
+    newCategory
+      .save()
+      .then(() => {
+        Category.find()
+          .select('title -_id')
+          .then(categories => {
+            res.status(201).json(categories);
+          })
+          .catch(err => {
+            res.status(500).json([{ error: err.message }]);
+          })
+      })
+      .catch(err => {
+        res.status(500).json([{ error: err.message }]);
+      });
+  });
+
+module.exports = router;
