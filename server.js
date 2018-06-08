58a957170aa2bc5c667b7fb7236eb5bb090b67e9
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-08 09:02:18 -0700

    server up and running and api connected

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express'); 
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+// remember to bring in my controllers
+const localHost = 'localhost:27017';
+const database = 'budgetdb';
 
 const server = express();
-
-// add your server code
-
 const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(`mongodb://${localHost}/${database}`)
+  .then(response => {
+      console.log("Connection Successful")
+  })
+  .catch(error => {
+      console.log("Connection Failed")
+  });
+
+server.use(helmet());
+server.use(express.json());
+// server.use('api/PLACEHOLDER', PLACEHOLDER_controller)
+
+server.get('/', (req, res) => {
+  res.status(200).json({ API: 'running' });
+});
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
