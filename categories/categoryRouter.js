a33633642835271064a1488b3e9835ceeb0129b0
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-08 10:50:10 -0700

    create all CRUD endpoints for categories

--- categories/categoryRouter.js
+++ categories/categoryRouter.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const Category = require('./Category.js')
+const Expense = require('../expenses/Expense.js')
 const router = express.Router()
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
 
 router
     .route('/')
@@ -24,6 +27,49 @@ router
             .select('title')
             .then( categories => {
                 res.status(200).json(categories)
+            })
+            .catch( err => {
+                res.status(500).json({ error: err.message })
+            })
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params
+        Category.findOne({ _id: { $eq: ObjectId(id) } })
+            .select('title -_id')
+            .then( category => {
+                Expense.find({ category: { $eq: ObjectId(id) } })
+                    .select('amount description')
+                    .then( expenses => {
+                        res.status(200)
+                            .json({...category._doc, expenses: expenses})
+                    })
+            })
+            .catch( err => {
+                res.status(500).json({ error: err.message })
+            })
+    })
+
+    .put((req, res) => {
+        const { id } = req.params
+        const newCategory = { title } = req.body
+        Category.findByIdAndUpdate(id, newCategory, { new: true })
+            .select('title -_id')
+            .then( category => {
+                res.status(200).json(category)
+            })
+            .catch( err => {
+                res.status(500).json({ error: err.message })
+            })
+    })
+
+    .delete((req, res) => {
+        const { id } = req.params
+        Category.findByIdAndRemove(id)
+            .then( category => {
+                res.status(200).json(category)
             })
             .catch( err => {
                 res.status(500).json({ error: err.message })
