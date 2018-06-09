201dab17cd5d64c8b7b11f0bb97eac280df9cb4d
JPEmery <clearkeyemery@gmail.com>
2018-06-09 11:40:37 -0500

    JP Emery: added server boilerplate and dependencies

---
+++ category/category.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+
+const Category = new mongoose.Schema({
+    title: String,
+})
+
+module.exports = mongoose.model('Category', Category);
--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+
+// add your server code
+
+const budgetRouter = require('./budget/budgetrouter.js')
+const categoryRouter = require('./category/categoryrouter.js')
+const expenseRouter = require('./expense/expenserouter.js')
 
 const server = express();
 
-// add your server code
+module.exports = {
+  connectTo: function(database = 'sandbox', host = 'localhost') {
+    return mongoose.connect(`mongodb://${host}/${database}`)
+  }
+}
+
+server.use(helmet());
+server.use(express.json())
+
+server.use('/api/budget', budgetRouter)
+server.use('/api/category', categoryRouter)
+server.use('/api/expense', expenseRouter)
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
