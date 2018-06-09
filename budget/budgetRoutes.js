58fe8f624609a255d9e8dbc56334cb1dff77b37f
Lisa Canini <three_nineteen@yahoo.com>
2018-06-09 11:20:39 -0700

    get and post to budget working

--- budget/budgetRoutes.js
+++ budget/budgetRoutes.js
@@ -5,13 +5,27 @@ const Budget = require('./Budget.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Budget.find()
-        .then(res => {
-            res.status(200).json(res)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
+    // Budget.find()
+    //     .then(budget => {
+            res.send('Working!!!')
+            // res.status(200).json({message: 'Working'})
+        // })
+        // .catch(err => {
+        //     res.status(500).json(err)
+        // })
 })
+
+// router.post('/', (req, res) => {
+//     const { title, budgetAmount } = req.body;
+//     const budget = new Budget(req.body);
+
+//     Budget.create(budget)
+//         .then(newBudget => {
+//             res.status(200).json(newBudget)
+//         })
+//         .catch(err => {
+//             res.status(500).json(err);
+//         })
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const db = require('./data/db');
-const budgetRoutes = require('./budget/Budget.js');
+// const budgetRoutes = require('./budget/Budget.js');
 const categoryRoutes = require('./category/Category.js');
 const expenseRoutes = require('./expense/Expense.js');
+const Budget = require('./budget/Budget')
 
 const server = express();
 
@@ -17,17 +19,35 @@ db
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/budget', budgetRoutes);
-server.use('/api/category', categoryRoutes);
-server.use('/api/expense', expenseRoutes);
+server.get('/api/budgets', (req, res) => {
+  Budget.find()
+      .then(budget => {
+  res.status(200).json(budget)
+  })
+  .catch(err => {
+      res.status(500).json(err)
+  })
 
-// server.get('/', (req, res) => {
-//   res.send('Working!!!')
-// })
+})
+server.post('/api/budgets', (req, res) => {
+    const { title, budgetAmount } = req.body;
+    const budget = new Budget(req.body);
 
-// server.post('/', (req, res) => {
-//   res.send('Working!!!')
-// })
+    Budget.create(budget)
+        .then(newBudget => {
+            res.status(200).json(newBudget)
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+// server.use('/api/budgets', budgetRoutes);
+// server.use('/api/categories', categoryRoutes);
+// server.use('/api/expenses', expenseRoutes);
+
+server.get('/', (req, res) => {
+  res.send('Working!!!')
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
