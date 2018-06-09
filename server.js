c4cc0e58bbc2a8d600e4cbccbcdb2cc549512e3c
Alexander <alexander.botello@g.austincc.edu>
2018-06-09 12:41:49 -0500

    Set up our server

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
-const express = require('express'); // remember to install your npm packages
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+
+const budgets = require('./budgets/router.js');
+const categories = require('./categories/router.js');
+const expenses = require('./expenses/router.js');
 
 const server = express();
 
-// add your server code
+mongoose.connect('mongodb://localhost/trackerdb')
+  .then(() => {
+    console.log('API CONNECTED TO DATABASE')
+  })
+  .catch(error => {
+    console.log(`ERROR CONNECTING TO DB, ${error}`)
+  })
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/budgets', budgets)
+server.use('/api/categories', categories)
+server.use('/api/expenses', expenses)
+
+server.get('/', (req, res) => {
+  res.send('API is running...')
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
