c0daebf17cc77bb802450608c2ba81451e3e7baf
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-08 20:39:03 -0400

    Created GET endpoint for budgets to return aggregated data for all budgets - Returns budgetAmount, totalExpenses and the amountReminaing

--- routes/budgets.js
+++ routes/budgets.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Budget = require('../models/budget');
+const Expense = require('../models/expense');
 
 router
   .post('/', (req, res) => {
@@ -10,6 +11,20 @@ router
     newBudget.save()
       .then(budget => {
         res.status(201).json(budget);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+  })
+  .get('/',  (req, res) => {
+    Expense.aggregate([
+      { $group: { _id: "$budget", total: { $sum: "$amount" } } },
+      { $lookup: { from: "budgets", localField: "_id", foreignField: "_id", as: "budgetDetails" } },
+      { $unwind: "$budgetDetails" },
+      { $project: { _id: 0, title: "$budgetDetails.title", budgetAmount: "$budgetDetails.budgetAmount", totalExpenses: "$total", amountRemaining: { $subtract: ["$budgetDetails.budgetAmount", "$total"] } } }
+    ])
+      .then(results => {
+        res.send(results);
       })
       .catch(error => {
         res.status(500).json(error);
