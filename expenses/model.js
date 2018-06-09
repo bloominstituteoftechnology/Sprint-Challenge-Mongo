debbd9b45d82b010c2bc20e8b05c9786b0ccdfe3
Alexander <alexander.botello@g.austincc.edu>
2018-06-09 13:28:00 -0500

    Add expenses model and router

---
+++ expenses/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const definition = {
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  budget: {
+    type: ObjectId,
+    ref: 'Budget'
+  },
+  category: {
+    type: ObjectId,
+    ref: 'Category'
+  }
+}
+
+const Schema = mongoose.Schema(definition);
+
+module.exports = mongoose.model("Expense", Schema, 'expenses')
---
+++ expenses/router.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const Expense = require('./model.js');
+
+const router = express.Router();
+
+router.route('/')
+  .get((req, res) => {
+    Expense.find().populate()
+     .then(expenses => {
+       res.status(200).json(expenses)
+     })
+     .catch(error => {
+      res.status(500).json(error);
+     })
+  })
+  .post((req, res) => {
+    const data = req.body;
+    const expense = new Expense(data);
+
+    expense.save()
+      .then(expense => {
+        res.status(201).json(expense);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+  })
+
+module.exports = router;
