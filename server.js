108105f06fe3899ddf693711007cbe93d0deba90
sbrooks84591 <Sbrooks84591@hotmail.com>
2018-06-09 13:00:49 -0500

    Category.post working, get will not filter

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const db = mongoose.connect('mongodb://localhost/budget')
 .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
 
 const Budget = require('./budgetModel');
+const Category = require('./categoryModel');
+
 server.use(express.json());
 // ENDPOINTS
 
@@ -33,6 +35,41 @@ server.post('/budgets', (req, res) => {
   })
 })
 
+/* `POST to '/categories'`
+
+* to `create` a category you should have a `'post'` method that stores the
+  category information.
+* you can write a `getter` `'get'` method that simply returns all the
+  categories. Filter out any unuseful information here, meaning we just want
+  the title of the categories.
+* create a minimum of 4 categories so that when you create your expenses, you
+  can assign where they go!
+* example of categories could be: `Food/Dining` `Gas/Auto` `Date Nights`
+  `Mortgage` */
+
+
+  server.post('/categories', (req, res) => {
+    const { title } = req.body;
+    const category = new Category({ title });
+    Category.create(category)
+    .then(saveCategory => {
+      res.send(saveCategory);
+      })
+      .catch(error => {
+        res.status(500).json({error:' Error: new category not saved to database'})
+  });
+});
+
+server.get('/categories', (req, res) => {
+  let query = Category.find()
+ // query.select({ id: -_id, v: -_v })
+  .then(category => {
+    res.send(category);
+  })
+  .catch(error => {
+    res.send(error);
+  });
+});
 
 
 
@@ -40,3 +77,4 @@ const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
+  
