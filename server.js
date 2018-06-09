e87dd1b000d5efafe6f489c3586501fe4e07af45
sbrooks84591 <Sbrooks84591@hotmail.com>
2018-06-09 11:43:15 -0500

    Server working, connected to db

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express'); // remember to install your npm packages
+const mongoose = require('mongoose')
+
 
 const server = express();
 
 // add your server code
+
+mongoose.connect('mongodb://localhost/budget')
+
+const db = mongoose.connect('mongodb://localhost/budget')
+.then(() => console.log('\n... API Connected to Database ...\n'))
+.catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+
+server.use(express.json());
+
+
+
+
+
+
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
