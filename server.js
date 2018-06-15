419c2cbd08d5a24138efb46c5f304cb7014186a3
JSNavey <jsnavey@gmail.com>
2018-06-15 11:59:47 -0400

    draft what server.js should look like

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express'); // remember to install your npm packages
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const server = express();
 
+//const budgetRouter = require('./budget/budgetRouter.js');
+//const catagoryRouter = require('./catagory/catagoryRouter.js');
+//const expenseRouter = require('./expense/expenseRouter.js');
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
 // add your server code
+//server.use('/api/budgets', budgetRouter);
+//server.use('/api/catagories', catagoryRouter);
+//server.use('/api/expenses', expenseRouter);
+
+server.get('/', (req,res) => res.send('API Running...'))
+
+//need to connect mongoose here
+// mongoose.connect('mongodb://localhost/budgetTracker', {}, (error) => {
+//   if(error) console.log(error);
+//   console.log('Mongoose connected us to our DB');
+// })
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
