e11a6add3e904744aa2fe12f9e1083ee9136042b
JSNavey <jsnavey@gmail.com>
2018-06-15 13:58:13 -0400

    added expenses

--- expense/expenseRouter.js
+++ expense/expenseRouter.js
@@ -4,6 +4,33 @@ const router = express.Router();
 const Expense = require('./Expense.js');
 
 //add endpoints
+router
+    .route('/')
+    .get((req, res) => {
+        Expense
+            .find()
+            .then(expenses => {
+                res.status(200).json({ expenses })
+            })
+            .catch(error => {
+                res.status(500).json({ error: error.message })
+            })
+})
 
+
+router
+    .route('/')
+    .post((req, res) => {
+        const { amount, description, budget, category } = req.body;
+        const newExpense = new Expense({ amount, description, budget, category });
+        newExpense
+            .save()
+            .then(newExpense => {
+                res.status(201).json({ newExpense })
+            })
+            .catch(error => {
+               res.status(500).json({ error: error.message })
+            })
+})
 
 module.exports = router;
