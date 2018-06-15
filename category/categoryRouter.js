71e1717f6b6c492c199147ec2b7d89dcc2e4f964
kammagik <kammagik@yahoo.com>
2018-06-15 10:25:12 -0700

    category

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -6,11 +6,32 @@ const router = express.Router();
 router 
 .route('/')
 .get((req, res) => {
-
+    Category.find()
+    .select('title')
+    .then(categories => {
+        res.status(200)
+        res.json({ budgets })
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
 })
+.post ((req, res) => {
+   const {title} = req.body;
+   const newCategory = new Category({title});
 
-.post((req, res) => {
-
+   if(!title){
+       res.status(400).json(error)
+   } else {
+       newCategory 
+       .save()
+       .then(Category => {
+           res.status(200)
+           res.json({Category})
+       })
+       .catch(err => {
+           res.status(500).json(error)
+       })
+   }
 })
-
 module.exports = router
