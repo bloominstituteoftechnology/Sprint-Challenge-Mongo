3c8a4e009c10643a53c24468748ef7ae1294ff8a
Tramane Hall <tramanedhall@gmail.com>
2018-06-15 12:49:01 -0400

    created expenseModel/Router.js

---
+++ expense/expenseModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+
+// {
+//     _id: ObjectId('503c2b66bcf86cs793443564'),
+//     amount: 35,
+//     description: 'potatoes',
+//     budget: ObjectId('507f1f77bcf86cd799439011'), // Monthly Spending
+//     category: ObjectId('543d2c72gsb23cd657438921') // Groceries
+// }
+
+const Expense = new mongoose.Schema({
+
+    amount: {
+        type: Number,
+        required: true
+    },
+
+    description: {
+        type: String,
+        require: true
+    },
+
+    budget: [{
+        type: ObjectId,
+        ref: "Budget",
+        required: true
+    }],
+    
+    category: [{
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    }]
+});
+
+
+
+
+
+
+module.exports = mongoose.model("Expense", Expense);
