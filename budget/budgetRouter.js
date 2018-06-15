1ec77594c076404385c46a5d89e1ee9ecafbb4a8
kammagik <kammagik@yahoo.com>
2018-06-15 10:13:04 -0700

    fixed budget router

--- budget/budgetRouter.js
+++ budget/budgetRouter.js
@@ -5,19 +5,31 @@ const router = express.Router();
 
 router 
 .route('/')
+.get((req, res) => {
+    Budget.find()
+    .then(budgets => {
+            res.status(200)
+            res.json({ budgets })
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+    })
 .post((req, res) => {
     const { budget, title } =req.body;
     const newBudget = new Budget({title, budget});
-
+    if(!title || !budget) {
+        res.status(400).json(error)
+    } else {
     newBudget
     .save()
-    .then(budget => {
+    .then(budgets => {
         res.status(200)
-        res.json({ budget })
+        res.json({ budgets })
     })
     .catch(error => {
         res.status(500).json(error)
-    })
+    })}
 })
 
 module.exports = router
