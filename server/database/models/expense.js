4c043a03a0f1f1310cf5eb1c4f78b2210377e9ef
FireInjun <Fireinjun291@gmail.com>
2018-06-22 19:34:21 -0400

    updated expense schema
    to allow for multiple budgets per expense

--- server/database/models/expense.js
+++ server/database/models/expense.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Expense = mongoose.Schema( {
     amount: { type: Number, required: true, default: 0 },
     description: { type: String, required: true, default: "Something"},
-    budget: { type: ObjectId, ref: 'Budget' },
+    budget: [{ type: ObjectId, ref: 'Budget' }],
     category: { type: ObjectId, ref: 'Category' },
 } );
 
