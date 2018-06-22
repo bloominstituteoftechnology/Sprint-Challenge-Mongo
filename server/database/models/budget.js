b2227ca7747c6e6666d6f8d7806449d92134b692
FireInjun <Fireinjun291@gmail.com>
2018-06-22 17:02:20 -0400

    Cleaned up Schemas
    To look more professional

--- server/database/models/budget.js
+++ server/database/models/budget.js
@@ -1,8 +1,8 @@
 const mongoose = require( 'mongoose' );
 
 const Budget = new mongoose.Schema( {
-    title: String,
-    budgetAmount: Number,
+    title: { type: String, required: true },
+    budgetAmount: { type: Number, default: 0 }
 } );
 
 module.exports = mongoose.model( 'Budget', Budget );
--- server/database/models/expense.js
+++ server/database/models/expense.js
@@ -2,8 +2,8 @@ const mongoose = require( 'mongoose' );
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Expense = mongoose.Schema( {
-    amount: { type: Number, required: true },
-    description: { type: String, required: true },
+    amount: { type: Number, required: true, default: 0 },
+    description: { type: String, required: true, default: "Something"},
     budget: { type: ObjectId, ref: 'Budget' },
     category: { type: ObjectId, ref: 'Category' },
 } );
