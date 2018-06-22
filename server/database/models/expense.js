c7e56248bfef610543dbac8d643a239657f3386e
FireInjun <Fireinjun291@gmail.com>
2018-06-22 19:35:53 -0400

    updated expense schema
    to allow for multiple categories

--- server/database/models/expense.js
+++ server/database/models/expense.js
@@ -5,7 +5,7 @@ const Expense = mongoose.Schema( {
     amount: { type: Number, required: true, default: 0 },
     description: { type: String, required: true, default: "Something"},
     budget: [{ type: ObjectId, ref: 'Budget' }],
-    category: { type: ObjectId, ref: 'Category' },
+    category: [{ type: ObjectId, ref: 'Category' }],
 } );
 
 module.exports = mongoose.model( 'Expense', Expense );
