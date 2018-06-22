f042190fc964c6fae75f5f798483862d1c617eb1
FireInjun <Fireinjun291@gmail.com>
2018-06-22 17:23:40 -0400

    Changed API addresses

--- server/server.js
+++ server/server.js
@@ -21,9 +21,9 @@ server.use( helmet() );
 server.use( express.json() );
 server.use( cors() );
 
-server.use( '/api/budget', budgetRouter );
-server.use( '/api/category', categoryRouter );
-server.use( '/api/expense', expenseRouter );
+server.use( '/api/budgets', budgetRouter );
+server.use( '/api/categories', categoryRouter );
+server.use( '/api/expenses', expenseRouter );
 
 
 server.get( '/', ( req, res ) => res.send( 'API Running...' ) );
