93a5a75c063f69bc7d9195ac60bc9f467228c8b1
FireInjun <Fireinjun291@gmail.com>
2018-06-22 18:46:57 -0400

    fixed Category
    CRUD endpoints

--- server/database/routes/categoryRouter.js
+++ server/database/routes/categoryRouter.js
@@ -38,55 +38,54 @@ router
     } )
 
 
-// router
-//     .route( '/:id' )
-//     .get( ( req, res ) =>
-//     {
-//         const { id } = req.params;
-//         Budget
-//             .findById( id )
-//             .then( foundBudget =>
-//             {
-//                 res.status( 200 ).json( foundBudget );
-//             } )
-//             .catch( err =>
-//             {
-//                 res.status( 500 ).json( { errorMessage: "That budget does not seem to be here." } );
-//             } )
-//     } )
+router
+    .route( '/:id' )
+    .get( ( req, res ) =>
+    {
+        const { id } = req.params;
+        Category
+            .findById( id )
+            .then( foundCategory =>
+            {
+                res.status( 200 ).json( foundCategory );
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { errorMessage: "I aint got a Category called that...." } );
+            } )
+    } )
 
-//     .put( ( req, res ) =>
-//     {
-//         const { id } = req.params;
-//         const updates = ( { firstName, lastName, age } = req.body );
-//         // findByIdAndUpdate
-//         Friend
-//             .findByIdAndUpdate( id, updates, { firstName, lastName, age } = req.body )
-//             .then( friend =>
-//             {
-//                 res.json( friend );
-//             } )
-//             .catch( err =>
-//             {
-//                 res.status( 500 ).json( { status: 'error didnt find what your looking for' } );
-//             } )
-//         // res.json(200).json({ status: 'please implement PUT functionality' });
-//     } )
-//     .delete( ( req, res ) =>
-//     {
-//         const { id } = req.params;
-//         const updates = ( { firstName, lastName, age } = req.body );
-//         // findByIdAndUpdate
-//         Friend
-//             .findByIdAndRemove( id, updates, { firstName, lastName, age } = req.body )
-//             .then( friendRemoved =>
-//             {
-//                 res.json( friendRemoved );
-//             } )
-//             .catch( err =>
-//             {
-//                 res.status( 500 ).json( { status: 'error didnt find what your looking for' } );
-//             } )
-//         // res.json(200).json({ status: 'please implement PUT functionality' });
-//     } )
+    .put( ( req, res ) =>
+    {
+        const { id } = req.params;
+        const { title } = req.body;
+        Category
+            .findByIdAndUpdate( id, { title } )
+            .then( category =>
+            {
+                res.json( category );
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { status: 'I aint gonna be able to update what aint here buddy!' } );
+            } )
+        // res.json(200).json({ status: 'please implement PUT functionality' });
+    } )
+    .delete( ( req, res ) =>
+    {
+        const { id } = req.params;
+        // const updates = ( { title } = req.body );
+        // findByIdAndUpdate
+        Category
+            .findByIdAndRemove( id /*, updates, { title } = req.body*/ )
+            .then( categoryRemoved =>
+            {
+                res.json( categoryRemoved );
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { status: 'That Category aint here buddy. Check your bank account to make sure you still have money!' } );
+            } )
+        // res.json(200).json({ status: 'please implement PUT functionality' });
+    } )
 module.exports = router;
