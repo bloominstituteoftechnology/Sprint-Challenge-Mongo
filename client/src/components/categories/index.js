640b1faa76a324330591352243edcb14d42cad9c
FireInjun <Fireinjun291@gmail.com>
2018-06-22 20:29:47 -0400

    Added Categories Components
    for CRUD operations

---
+++ client/src/components/categories/index.js
@@ -0,0 +1,8 @@
+import CategorysList from './CategorysList';
+import CategoryView from './CategoryView';
+import CreateCategory from './CreateCategory';
+import EditCategory from './EditCategory';
+import DeleteCategory from './DeleteCategory';
+
+
+export { CreateCategory, DeleteCategory, EditCategory, CategorysList, CategoryView }
