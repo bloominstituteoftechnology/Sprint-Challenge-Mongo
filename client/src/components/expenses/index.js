2b832d9dff9424300e183c4039e73845d65adf94
FireInjun <Fireinjun291@gmail.com>
2018-06-22 20:29:12 -0400

    Added expenses Components
    for CRUD functionality

---
+++ client/src/components/expenses/index.js
@@ -0,0 +1,8 @@
+import ExpensesList from './ExpensesList';
+import ExpenseView from './ExpenseView';
+import CreateExpense from './CreateExpense';
+import EditExpense from './EditExpense';
+import DeleteExpense from './DeleteExpense';
+
+
+export { CreateExpense, DeleteExpense, EditExpense, ExpensesList, ExpenseView }
