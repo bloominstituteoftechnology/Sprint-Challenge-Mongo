8d029e127c48f7dcfab61c9aa0bd0f8fe6228214
FireInjun <Fireinjun291@gmail.com>
2018-06-22 20:28:38 -0400

    added Budget Components
    for CRUD functionality

---
+++ client/src/components/budgets/index.js
@@ -0,0 +1,8 @@
+import BudgetsList from './BudgetsList';
+import BudgetView from './BudgetView';
+import CreateBudget from './CreateBudget';
+import EditBudget from './EditBudget';
+import DeleteBudget from './DeleteBudget';
+
+
+export { CreateBudget, DeleteBudget, EditBudget, BudgetsList, BudgetView }
