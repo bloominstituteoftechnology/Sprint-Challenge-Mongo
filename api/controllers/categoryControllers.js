ed92297984f2aebf77f7746b245e03f56c2c0efe
Mark Marchant <jtla3@yahoo.com>
2018-07-27 14:41:18 -0700

    categoryControllers done

---
+++ api/controllers/categoryControllers.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Category = require('../models/category');
+
+const createCategory = (req, res) => {
+    const category = new Category(req.body);
+
+    category
+        .save()
+        .then(newCategory => {
+            res.status(201).json(newCategory);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error createing category', err });
+        });
+};
+
+const getCategories = (req, res) => {
+    Category.find({})
+        .then(categories => {
+            res.status(200).json(categories);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error getting categories', err });
+        });
+};
+
+module.exports = {
+    createCategory,
+    getCategories
+};
