4e3e59d65532de653835bab5fe2d7092a99dcc66
Mark Marchant <jtla3@yahoo.com>
2018-07-27 21:12:34 -0700

    Routes are set up

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,3 +1,26 @@
+const express = require('express');
+
+const budgetControllers = require("../controllers/budgetControllers");
+const categoryControllers = require("../controllers/categoryControllers");
+const expenseControllers = require("../controllers/expenseControllers")
+
 module.exports = app => {
+  const router = express.Router();
   // Todo: Fill in your routes here
+
+  // Budget GET and POST routes
+  router.route('/budget').post(budgetControllers.createBudget);
+  router.route("/budget/:id/summary").get(budgetControllers.getBudgetSummary);
+  router.route('/budget').get(budgetControllers.getBudgets);
+
+  // Category GET and POST routes
+  router.route('/category').post(categoryControllers.createCategory);
+  router.route('/category').get(categoryControllers.getCategories);
+
+  // Expenses GET and POST routes
+  router.route('/expense').post(expenseControllers.createExpense);
+  router.route('/expense').get(expenseControllers.getExpenses);
+  router.route('/expenses').get(expenseControllers.getCategoryExpenses);
+
+  app.use('/api', router);
 };
