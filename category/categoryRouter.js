fd1bcddce89168ad3372ca14318e9dd1abe94155
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-04 23:02:17 -0700

    fix: categoryRouter.js
     - check categoryRouter.js
     - POST and GET works fine
     - created four categories in DB

--- category/categoryRouter.js
+++ category/categoryRouter.js
@@ -4,19 +4,9 @@ const Category = require('./category');
 
 router
     .route('/')
-    .get((req, res) => {
-       Category.find()
-       .select('title')
-       .then(entriesCategory => {
-            res.status(200).json(entriesCategory);
-        })
-        .catch(err => res.status(500).json({error: 'Error fetching category'}))
-    })
-
     .post((req, res) => {
         const { title } = req.body;
         const newCategory = new Category({ title });
-        console.log(newCategory);
         newCategory
             .save()
             .then(savedCategory => {
@@ -25,6 +15,16 @@ router
             .catch(err => {
                 res.status(500).json({ error: 'Error saving category'});
             });
-    });
+    })
+    .get((req, res) => {
+       Category
+        .find()
+        .select('title')
+        .then(entriesCategory => {
+            res.status(200).json(entriesCategory);
+        })
+        .catch(err => res.status(500).json({error: 'Error fetching category'}))
+    })
+
 
     module.exports = router;
