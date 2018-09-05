d80e7830d13fe2570b026e407e134be78084d9ad
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-04 23:00:22 -0700

    feat: fix server.js
     - restructured code.
     - fixed routes and typo.

--- server.js
+++ server.js
@@ -3,31 +3,32 @@ const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 
-const server = express();
-
-mongoose
-  .connect('mongodb://localhost/budgetdb')
-  .then(() => console.log('Connected to DB'))
-  .catch(error => console.log('Error connecting to DB', error))
-
-server.use(express.json());
-server.use(cors());
-server.use(helmet());
-
 const budgetRouter = require('./budget/budgetRouter.js');
 const expenseRouter = require('./expense/expenseRouter.js');
 const categoryRouter = require('./category/categoryRouter.js');
 
-server.use('/api/budget', budgetRouter);
-server.use('/api/expense', expenseRouter);
-server.use('/api/category', categoryRouter);
+const server = express();
 
+const port = process.env.PORT || 5000;
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.status(201).json('Server Running')
 });
 
-const port = process.env.PORT || 5000;
+server.use('/api/budgets', budgetRouter);
+server.use('/api/expenses', expenseRouter);
+server.use('/api/categories', categoryRouter);
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect('mongodb://localhost/budgetdb')
+  .then(() => console.log('Connected to DB'))
+  .catch(error => console.log('Error connecting to DB', error))
+
 server.listen(port, () => {
   console.log(`Server up and running on ${port}`);
 });
