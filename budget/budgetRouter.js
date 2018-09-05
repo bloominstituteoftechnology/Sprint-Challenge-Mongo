3797e6c830cd0d24ec373d6c5281f80b4ec9d8e8
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-04 22:41:12 -0700

    feat: restart repor for MBP
     - fixed budgetRouter.js

--- budget/budgetRouter.js
+++ budget/budgetRouter.js
@@ -3,20 +3,10 @@ const router = require('express').Router();
 const Budget= require('./budget');
 
 router
-    // .route('/')
-    // .get((req, res) => {
-    //     Budget
-    //     .find()
-    //     .then(response => {
-    //         res.status(200).json(budget);
-    //     })
-    //     .catch(err => res.status(500).json({error: 'Error fetching budget'}))
-    // })
     .route('/')
     .post((req, res) => {
         const { title, budgetAmount } = req.body;
         const newBudget = new Budget({ title, budgetAmount });
-        console.log(newBudget);
         newBudget
             .save()
             .then(savedBudget => {
