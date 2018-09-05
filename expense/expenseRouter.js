9c7cd9bdd43db480c3fe310748dde703d94b4391
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-04 23:26:01 -0700

    fix: fixed expenseRouter.js
     - added 2nd arguments in populate

--- expense/expenseRouter.js
+++ expense/expenseRouter.js
@@ -4,22 +4,22 @@ const Expense = require('./expense');
 
 router
     .route('/')
-        .get((req, res) => {
-            Expense
-                .find()
-                    .populate('budget')
-                    .populate('category')
-                    .then(foundExpense => res.json(foundExpense))
-                    .catch(err => res.status(500).json({error: err}));
-                })
+    .get((req, res) => {
+        Expense
+            .find()
+            .populate('budget', 'title -_id')
+            .populate('category', 'title -_id')
+            .then(foundExpense => res.json(foundExpense))
+            .catch(err => res.status(500).json({error: err}));
+            })
             
-        .post((req, res) => {
-            const { amount, description, budget, category } = req.body;
-            const newExpense = new Expense({ amount, description, budget, category });
-            newExpense
-                .save()
-                .then(expenses => res.json(expenses))
-                .catch(err => res.status(500).json({error: err}));
+    .post((req, res) => {
+        const { amount, description, budget, category } = req.body;
+        const newExpense = new Expense({ amount, description, budget, category });
+        newExpense
+            .save()
+            .then(expenses => res.json(expenses))
+            .catch(err => res.status(500).json({error: err}));
         })
 
 module.exports = router;
